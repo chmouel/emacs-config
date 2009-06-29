f6a8f86e72b0d359c0e14cd4a90571679d645df2
Chmouel Boudjnah <chmouel.boudjnah@rackspace.co.uk>
2009-06-28 22:33:45 -0500

    remove sensitive information.

--- hosts/cboudjna.el
+++ hosts/cboudjna.el
@@ -1,6 +1,4 @@
 ;Env
-(setenv "MOSSO_API_USER" "chmouelb")
-(setenv "MOSSO_API_KEY" "c3599eb5093d7f598a644640cfe5f3ae")
 (setenv "GIT_SSH" "c:/PROGRA~1/PuTTY/plink.exe")
 
 ;Coding make sure it's unix
