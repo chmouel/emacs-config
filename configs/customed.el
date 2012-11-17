a07bd87fd83339a9397029b4e1c574f3b63035f8
Chmouel Boudjnah <chmouel@chmouel.com>
2012-11-18 09:04:27 +1000

    Updates and cleanup.

--- configs/customed.el
+++ configs/customed.el
@@ -171,5 +171,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-item-highlight ((t (:weight bold))))
- '(which-func ((t (:weight bold)))))
+ '(magit-item-highlight ((t (:weight bold))) t)
+ '(which-func ((t (:weight bold))) t))
--- configs/settings.el
+++ configs/settings.el
@@ -8,8 +8,6 @@
 (global-set-key '[(control tab)]            'other-window)
 (global-set-key '[(control x)(control =)]   'ediff-buffers)
 (global-set-key '[(control x)(-)] 'next-error)
-(global-set-key (read-kbd-macro "C-=") 'jump-to-register)
-(global-set-key (read-kbd-macro "C-M-=") 'window-configuration-to-register)
 (global-set-key (read-kbd-macro "C-M-g") 'grep)
 (global-set-key (read-kbd-macro "C-M-b") 'bookmark-jump)
 (global-set-key (read-kbd-macro "C-x C-k") (lambda () (interactive) (kill-buffer nil)))
