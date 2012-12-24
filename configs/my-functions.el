818ac2951c23a5024c8e882f7a260aea4333eb01
Chmouel Boudjnah <chmouel@chmouel.com>
2012-12-24 08:16:38 +0200

    From http://whattheemacsd.com//key-bindings.el-01.html
    What are those line numbers for anyway?
    
    (global-set-key [remap goto-line] 'goto-line-with-feedback)
    
    (defun goto-line-with-feedback ()
      "Show line numbers temporarily, while prompting for the line number input"
      (interactive)
      (unwind-protect
          (progn
            (linum-mode 1)
            (goto-line (read-number "Goto line: ")))
        (linum-mode -1)))
    I don't have line numbers visible in the fringe of my Emacs. If I want to go to a line number, that is usually because it is referenced in an error message somewhere. Showing them all the time is just noise to me.
    
    Still, many people want line numbers visible. I guess that is because they use them for navigation. This snippet shows line numbers temporarily just when you're going to a line number with goto-line.
    
    Notice the nice remap-trick in the key binding. It will remap all key bindings from goto-line to goto-line-with-feedback. Neat!

--- configs/my-functions.el
+++ configs/my-functions.el
@@ -144,3 +144,17 @@
      (darwin "open"))
    nil
    (dired-get-marked-files t current-prefix-arg)))
+
+
+;; Remap goto-line to show temporary the line number.
+;; http://whattheemacsd.com//key-bindings.el-01.html
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
