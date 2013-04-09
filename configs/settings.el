810ba9f15dff720f5adf0f5d2d858be95bbb4d99
Chmouel Boudjnah <chmouel@chmouel.com>
2013-04-09 21:21:07 +0200

    Too soon.

--- configs/settings.el
+++ configs/settings.el
@@ -67,16 +67,3 @@
   (read-kbd-macro "C-p") 'previous-history-element)
 (define-key minibuffer-local-map
   (read-kbd-macro "C-n") 'next-history-element)
-
-(global-unset-key (kbd "<left>"))
-(global-unset-key (kbd "<right>"))
-(global-unset-key (kbd "<up>"))
-(global-unset-key (kbd "<down>"))
-(global-unset-key (kbd "<C-left>"))
-(global-unset-key (kbd "<C-right>"))
-(global-unset-key (kbd "<C-up>"))
-(global-unset-key (kbd "<C-down>"))
-(global-unset-key (kbd "<M-left>"))
-(global-unset-key (kbd "<M-right>"))
-(global-unset-key (kbd "<M-up>"))
-(global-unset-key (kbd "<M-down>"))
