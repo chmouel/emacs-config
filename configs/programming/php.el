027a83d594a82293ac7c968b7302994e65ed8ab4
Chmouel Boudjnah <chmouel@chmouel.com>
2013-06-02 10:52:30 +0200

    Make php-mode works and remove those c-n/p binding.

--- configs/programming/php.el
+++ configs/programming/php.el
@@ -1,11 +1,12 @@
-(autoload 'php-mode "php-mode" nil t)
+(push "~/.emacs.d/packages/php-mode" load-path)
+(autoload 'php-mode "php-mode" "PHP Mode" t)
 (setq auto-mode-alist (cons '("\\.php\\'" . php-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.inc\\'" . php-mode) auto-mode-alist))
 
 (defun my-php-mode-hook ()
   (set (make-local-variable 'my-compile-command) (concat "php -l \"" buffer-file-name "\""))
   (set (make-local-variable 'my-compile-run-command) (concat "php \"" buffer-file-name "\""))
-  
+
   (c-set-style "awk")
   (c-toggle-auto-newline 1)
 
@@ -13,9 +14,8 @@
 
   (local-set-key '[(tab)] 'indent-for-tab-command)
   (local-set-key '[(control c)(control v)] 'browse-url-of-buffer)
-  (local-set-key '[(control =)] 'html-mode)  
-  (local-set-key '[(control n)] (lambda () (interactive) (progn (insert "\\n"))))
-  (local-set-key '[(control p)] (lambda () (interactive) (progn (insert "<?=  ?>")(backward-char 3))))
+  (local-set-key '[(control shift n)] (lambda () (interactive) (progn (insert "\\n"))))
+  (local-set-key '[(control shift p)] (lambda () (interactive) (progn (insert "<?=  ?>")(backward-char 3))))
   (local-set-key '[(f12)] 'html-inlined-narrow))
 
 (add-hook 'php-mode-hook 'my-php-mode-hook)
