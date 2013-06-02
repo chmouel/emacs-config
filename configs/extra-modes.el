1d80af32cc57079dca7701497706a6dd49d15567
Chmouel Boudjnah <chmouel@chmouel.com>
2013-06-02 22:56:33 +0200

    Cleanup.

--- configs/extra-modes.el
+++ configs/extra-modes.el
@@ -116,10 +116,3 @@
 (my-ensure-installed 'window-number)
 (autoload 'window-number-meta-mode "window-number")
 (window-number-meta-mode 't)
-
-(my-ensure-installed 'window-number)
-(eval-after-load "erc"
-  '(require 'erc-hl-nicks))
-
-(autoload 'znc-all "znc" "JSON Mode" t)
-(autoload 'znc-erc "znc" "JSON Mode" t)
