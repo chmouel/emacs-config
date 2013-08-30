e045f79cd7424d8b48d6eed1b00275524f6e3acf
Chmouel Boudjnah <chmouel@chmouel.com>
2013-08-30 19:33:41 +0200

    auto-complete and c-x-c-e eval buffer.

--- configs/programming/elisp.el
+++ configs/programming/elisp.el
@@ -1,6 +1,8 @@
 ;Eldoc-Mode
 
 (defun my-emacs-lisp-mode-hook ()
+  (auto-complete-mode-maybe)
+  (local-set-key (read-kbd-macro "C-x C-e") 'eval-buffer)
   (eldoc-mode t)
   (my-programming-common-hook)
   )
