9cda2ce68fb939aef7ce83277894cd9484695ed1
Chmouel Boudjnah <chmouel@chmouel.com>
2013-09-02 08:19:12 +0200

    trying smartparens mode.

--- configs/programming/elisp.el
+++ configs/programming/elisp.el
@@ -1,6 +1,12 @@
 ;Eldoc-Mode
+(Package 'smartparens
+  (set-face-attribute 'sp-pair-overlay-face nil
+                      :background "steelblue3"
+                      :foreground "white"
+                      :box nil)  )
 
 (defun my-emacs-lisp-mode-hook ()
+  (smartparens-mode)
   (company-mode)
   (local-set-key (read-kbd-macro "C-x C-e") 'eval-buffer)
   (eldoc-mode t)
