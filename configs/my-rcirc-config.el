a201f34140e4ea9c56cc304be9104436d55d637a
Chmouel Boudjnah <chmouel@chmouel.com>
2013-09-05 10:09:37 +0200

    Switch to buffer on privmsg.

--- configs/my-rcirc-config.el
+++ configs/my-rcirc-config.el
@@ -55,3 +55,17 @@
            (window-configuration-to-register :rcirc-pre)
            (jump-to-register :rcirc)))))
 (global-set-key (read-kbd-macro "C-S-k") 'my-jump-rcirc)
+
+; Switch to buffer on private messages.
+(defun my-switch-to-buffer-on-privmsg (proc sender response target text)
+  (when (and (string= response "PRIVMSG")
+             (not (string= sender (rcirc-nick proc)))
+             (not (rcirc-channel-p target)))
+    (switch-to-buffer (rcirc-get-buffer proc target))
+    (goto-char rcirc-prompt-end-marker)))
+
+(add-hook 'rcirc-print-hooks 'my-switch-to-buffer-on-privmsg)
+
+
+; Notification
+(require 'rcirc-notify')
