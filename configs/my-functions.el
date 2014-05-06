8dda3883af0ed5e4e05d92f78f9403d87498b522
Chmouel Boudjnah <chmouel@enovance.com>
2014-05-06 08:04:21 +0200

    add my-rotate-window

--- configs/my-functions.el
+++ configs/my-functions.el
@@ -217,3 +217,29 @@
   (let ((fill-column (point-max)))
     (fill-region beg end)))
 (define-key global-map "\C-\M-Q" 'unfill-region)
+
+;https://github.com/milkypostman/dotemacs/blob/master/init.el
+(defun my-rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
