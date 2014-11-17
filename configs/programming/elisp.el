e09c797f526113d44b73f2b0020e362777d497c7
Chmouel Boudjnah <chmouel@enovance.com>
2014-11-17 12:53:02 +0100

    smartparens-mode for emacs-lisp

--- configs/programming/elisp.el
+++ configs/programming/elisp.el
@@ -1,10 +1,13 @@
 ;Eldoc-Mode
+(Package 'smartparens)
+
 (defun my-emacs-lisp-mode-hook ()
+  (smartparens-mode)
   (company-mode)
   (local-set-key (read-kbd-macro "C-x C-e") 'eval-buffer)
   (turn-on-eldoc-mode)
-  (my-programming-common-hook)
-  )
+  (my-programming-common-hook))
+
 (add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
