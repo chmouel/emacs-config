cf6a5f37be74da6d053ee57c20f069075b4cd3c7
Chmouel Boudjnah <chmouel@enovance.com>
2015-04-02 10:27:33 +0200

    remove some old stuff from magit config
    didn't need and updated to 1.4.0

--- configs/my-magit-config.el
+++ configs/my-magit-config.el
@@ -4,15 +4,7 @@
   (autoload 'magit-get-top-dir "magit" nil t)
   (global-set-key (read-kbd-macro "C-c g") 'magit-status)
 
-  ;; Full screen magit.
-  (defadvice magit-status (around magit-fullscreen activate)
-    (window-configuration-to-register :magit-fullscreen)
-    ad-do-it
-    (delete-other-windows))
-
-  (defadvice magit-quit-window (around magit-restore-screen activate)
-    ad-do-it
-    (jump-to-register :magit-fullscreen))
+  (setq magit-last-seen-setup-instructions "1.4.0")
 
   ;; Magit and git quick open files for projects.
   (defun my-magit-open-directory-and-files ()
@@ -28,11 +20,7 @@
     (dired (magit-read-top-dir nil)))
   (global-set-key (read-kbd-macro "C-S-o") 'my-magit-open-repository))
 
-(Package 'magit-gerrit)
-(eval-after-load "magit"
-  '(require 'magit-gerrit))
-
-;Find find in GIT repo
+;;Find find in GIT repo
 (Package 'magit-find-file
   (add-hook 'magit-mode-hook '(lambda () (interactive) (local-set-key '[(\`)] 'magit-find-file-completing-read)))
   (global-set-key (kbd "C-S-f") 'magit-find-file-completing-read))
