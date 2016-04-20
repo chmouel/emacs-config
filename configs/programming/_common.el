9bc8d69771acef00bfec3305897ba545aafc86fc
Chmouel Boudjnah <chmouel@chmouel.com>
2016-04-20 11:29:06 +0200

    redefine browse-github keybinding was conflicting
    with go-mode

--- configs/programming/_common.el
+++ configs/programming/_common.el
@@ -3,7 +3,7 @@
 (defun my-programming-common-hook()
   (highlight-numbers-mode 1)
   (local-set-key (kbd "RET") 'newline-and-indent)
-  (local-set-key (kbd "C-C C-f") 'github-browse-file)
+  (local-set-key (kbd "C-C C-f g") 'github-browse-file)
   (local-set-key '[(meta return)] 'compile)
   (local-set-key '[(control meta return)] (lambda () (interactive) (compile my-compile-command)))
   (local-set-key '[(control return)] (lambda () (interactive) (compile my-compile-run-command)))
