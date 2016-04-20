d29124923d55cab3b0419621a8e47661c97d141b
Chmouel Boudjnah <chmouel@chmouel.com>
2016-04-20 11:28:05 +0200

    Some updates to the customed changes with packages

--- configs/customed.el
+++ configs/customed.el
@@ -209,6 +209,9 @@
    (quote
     (("melpa-unstable" . "http://melpa.org/packages/")
      ("GNU" . "http://elpa.gnu.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (esqlite go-direx go-errcheck gore-mode go-snippets go-playground golint go-rename toggle-quotes zygospore zop-to-char zlc zerodark-theme zenburn-theme yasnippet yascroll yaml-mode window-number wgrep web virtualenvwrapper use-package try transpose-frame tox tablist tabbar smex smartparens smart-shift smart-mode-line shorten rpm-spec-mode request-deferred regex-tool recent-addresses rcirc-color rcirc-alertify queue python-docstring pyfmt powerline php-mode phi-search perspective per-buffer-theme pcsv pcache pastie paren-completer paredit pacmacs packed package-safe-delete pabbrev outline-magic org-magit org-download org-context org-cliplink org-bullets openstack-cgit-browse-file neotree names multiple-cursors message-x marmalade markdown-mode+ magit-find-file lui logito lcs langtool json-rpc json-mode jabber ivy isgd isearch-dabbrev inf-ruby ido-vertical-mode ido-completing-read+ ibuffer-vc hungry-delete htmlize highlight-numbers highlight-80+ helm-spotify google-this google-contacts go-eldoc gnus-desktop-notify github-browse-file gerrit-download fringe-helper flycheck flx-ido find-file-in-git-repo fill-column-indicator fakir expand-region esxml es-lib erc-hl-nicks edebug-x dropdown-list dockerfile-mode db csv-mode csharp-mode company-go company-anaconda comment-dwim-2 browse-kill-ring boxquote bm bitly bbdb auto-complete anything-git-goto ansible alect-themes aggressive-indent ag afternoon-theme)))
  '(package-user-dir "~/.emacs.d/packages")
  '(projectile-cache-file "~/.emacs.d/auto-save-list/projectile.cache")
  '(projectile-keymap-prefix "")
@@ -237,8 +240,8 @@
  '(rst-level-face-base-color "dark slate gray")
  '(safe-local-variable-values (quote ((c-hanging-comment-ender-p))))
  '(save-interprogram-paste-before-kill t)
- '(save-place t nil (saveplace))
  '(save-place-file "~/.emacs.d/auto-save-list/emacs-places")
+ '(save-place-mode t nil (saveplace))
  '(savehist-file "~/.emacs.d/auto-save-list/history")
  '(savehist-mode t)
  '(server-use-tcp nil)
