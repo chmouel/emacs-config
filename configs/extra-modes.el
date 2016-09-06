cace654fc51f2f8b64ac82994aa4559f69765229
Chmouel Boudjnah <chmouel@chmouel.com>
2016-09-06 08:18:33 +0200

    highlight-indentation

--- configs/extra-modes.el
+++ configs/extra-modes.el
@@ -160,5 +160,10 @@
 (Package 'google-this
   (global-set-key (kbd "s--") 'google-this))
 
+;;
+(Package 'highlight-indentation
+  (set-face-background 'highlight-indentation-face "#e3e3d3")
+  (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+  )
 
 (provide 'extras-modes)
