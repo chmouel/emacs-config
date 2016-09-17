acc1eec86a1c2814e035a44d1472a1aa35b204c7
Chmouel Boudjnah <chmouel@chmouel.com>
2016-09-17 14:56:39 +0200

    Highlight indentation

--- configs/customed.el
+++ configs/customed.el
@@ -288,4 +288,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-item-highlight ((t nil))))
+ '(magit-item-highlight ((t nil)))
+ '(which-func ((t (:foreground "cyan")))))
--- configs/extra-modes.el
+++ configs/extra-modes.el
@@ -162,8 +162,9 @@
 
 ;;
 (Package 'highlight-indentation
-  (set-face-background 'highlight-indentation-face "#e3e3d3")
-  (set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
-  )
+    (eval-after-load "highlight-indentation"
+      '(progn
+         (set-face-background 'highlight-indentation-face "#e3e3d3")
+         (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))))
 
 (provide 'extras-modes)
