2c4445b6c45f5a84bdf3903980a105c80fbf6037
Chmouel Boudjnah <chmouel@chmouel.com>
2016-12-10 09:32:07 +0100

    indentation

--- configs/programming/_common.el
+++ configs/programming/_common.el
@@ -5,6 +5,5 @@
   (local-set-key (kbd "RET") 'newline-and-indent)
   (local-set-key (kbd "C-C C-l") 'github-browse-file)
   (local-set-key '[(meta return)] 'compile)
-  (local-set-key '[(control meta return)] (lambda () (interactive) (compile my-compile-command)))
-  (local-set-key '[(control return)] (lambda () (interactive) (compile my-compile-run-command)))
-  )
+  (local-set-key '[(control meta return)] (lambda () (interactive) (compile (or compile-command my-compile-command))))
+  (local-set-key '[(control return)] (lambda () (interactive) (compile (or  compile-command my-compile-run-command)))))
