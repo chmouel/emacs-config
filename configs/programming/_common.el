f4a4badf1655dc0a81bf8579b288fba2782529e5
Chmouel Boudjnah <chmouel@chmouel.com>
2017-03-09 16:46:23 +0100

    add toggle-quotes

--- configs/programming/_common.el
+++ configs/programming/_common.el
@@ -1,9 +1,12 @@
 (Package 'highlight-numbers)
 
+(autoload 'toggle-quotes "toggle-quotes")
+
 (defun my-programming-common-hook()
   (highlight-numbers-mode 1)
   (local-set-key (kbd "RET") 'newline-and-indent)
   (local-set-key (kbd "C-C C-l") 'github-browse-file)
+  (local-set-key (kbd "C-'") 'toggle-quotes)
   (local-set-key '[(meta return)] 'compile)
   (local-set-key '[(control meta return)] (lambda () (interactive) (compile my-compile-command)))
   (local-set-key '[(control return)] (lambda () (interactive) (compile my-compile-run-command))))
