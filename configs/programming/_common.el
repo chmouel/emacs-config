677fbf1598b5423df3c1e8f90a7e9f65857a3ed7
Chmouel Boudjnah <chmouel@chmouel.com>
2017-03-13 09:50:51 +0100

    only set when boundp the variable

--- configs/programming/_common.el
+++ configs/programming/_common.el
@@ -8,5 +8,8 @@
   (local-set-key (kbd "C-C C-l") 'github-browse-file)
   (local-set-key (kbd "C-'") 'toggle-quotes)
   (local-set-key '[(meta return)] 'compile)
-  (local-set-key '[(control meta return)] (lambda () (interactive) (compile my-compile-command)))
-  (local-set-key '[(control return)] (lambda () (interactive) (compile my-compile-run-command))))
+
+  (if (fboundp 'my-compile-command)
+      (local-set-key '[(control meta return)] (lambda () (interactive) (compile my-compile-command))))
+  (if (fboundp 'my-compile-run-command)
+      (local-set-key '[(control return)] (lambda () (interactive) (compile my-compile-run-command)))))
