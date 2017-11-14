75da2b2d0b5f94bfca5d5f080ce34f288efbfe4d
Chmouel Boudjnah <chmouel@chmouel.com>
2017-11-14 07:22:27 +0100

    Org stuff

--- configs/my-orgmode-config.el
+++ configs/my-orgmode-config.el
@@ -7,6 +7,8 @@
  org-default-notes-file (concat org-directory "/notes.org")
  org-return-follows-link t)
 
+(require 'the-org-mode-expansions)
+
 (define-key global-map "\C-cc" 'org-capture)
 
 (setq org-link-abbrev-alist
@@ -20,8 +22,29 @@
          (file+headline  (concat org-directory "/links.org") "Links to Read")
          "* %?\n %i")))
 
+(defvar org-electric-pairs '((?\' . ?\')(?\* . ?\*) (?/ . ?/) (?= . ?=)
+                             (?\_ . ?\_) (?~ . ?~) (?+ . ?+)) "Electric pairs for org-mode.")
+
+(defun org-add-electric-pairs ()
+  (setq-local electric-pair-pairs (append electric-pair-pairs org-electric-pairs))
+  (setq-local electric-pair-text-pairs electric-pair-pairs))
+
+(add-to-list 'org-emphasis-alist
+             '("*" (:foreground "grey" :weight bold)))
+
 (defun my-org-mode-hook ()
+  (local-set-key (kbd "M-n") 'outline-next-visible-heading)
+  (local-set-key (kbd "M-p") 'outline-previous-visible-heading)
+  (local-set-key (kbd "C-c a") 'org-agenda)
+  (org-display-inline-images t t)
+  (org-add-electric-pairs)
   (auto-fill-mode)
   (electric-indent-mode)
   (flyspell-mode))
+
+(dolist (k
+         '("<S-down>" "<S-up>" "<S-left>"
+           "<S-right>" "<C-TAB>" "<M-up>"
+           "<M-down>"))
+  (define-key org-mode-map (kbd k) nil))
 (add-hook 'org-mode-hook 'my-org-mode-hook)
