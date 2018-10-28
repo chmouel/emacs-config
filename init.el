47663e4970d5bdbee888e0adb0926e26f6914486
Chmouel Boudjnah <chmouel@chmouel.com>
2018-10-28 05:38:04 +0100

    use-package-chords

--- init.el
+++ init.el
@@ -43,8 +43,12 @@
 ;; use-package
 (if (not (fboundp 'use-package))
     (package-install 'use-package))
-(require 'use-package)
+(eval-when-compile (require 'use-package))
 (setq use-package-always-ensure t)
+
+;; use-package key chord
+(use-package use-package-chords
+  :config (key-chord-mode 1))
 
 ;; Loading Configs files
 (my-load-dir (concat my-init-directory "/configs/"))
