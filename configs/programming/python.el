a8039796d76b7fcd0668bb2bbaf28b33a8144b19
Chmouel Boudjnah <chmouel@chmouel.com>
2019-11-26 23:36:12 +0100

    add my-py-import
    about time i coded it

--- configs/programming/python.el
+++ configs/programming/python.el
@@ -18,14 +18,33 @@
  python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
-(reformatter-define yapf :program "yapf")
+;; Insert an import
+(defun my-py-insert-import (arg import)
+  (save-excursion
+    (goto-char (point-min))
+    (unless
+        (re-search-forward
+         (concat "^\\(import\\|from.*import\\) " import) nil t)
+      (goto-char (point-min))
+      (when (re-search-forward "^\\([^#]\\)" nil t)
+        (beginning-of-line)
+        (insert
+         (concat
+          "import " import
+          (if arg (concat " as " (read-from-minibuffer "Import as: "))))
+         "\n")))))
 
+(defun my-py-import-add (arg import)
+  (interactive
+   (list
+    current-prefix-arg
+    (read-from-minibuffer "Package: " )))
+  (my-py-insert-import arg import))
+
+(reformatter-define yapf :program "yapf")
 (defun my-python-mode-hook()
   (setq flycheck-python-flake8-executable python-shell-interpreter)
   (yapf-on-save-mode)
-
-  ;; Ipython
-  ;; (blacken-mode)
   (fci-mode)
   (flycheck-mode)
   (anaconda-mode)
@@ -36,10 +55,10 @@
   (local-set-key '[(meta q)] 'fill-paragraph)
   (local-set-key (kbd "C-'") 'toggle-quotes)
 
+  (local-set-key '[(control c)(i)] 'my-py-import-add)
   (local-set-key '[(control c)(\[)] 'flycheck-previous-error)
   (local-set-key '[(control c)(\])] 'flycheck-next-error)
 
   (setq-local compile-command (concat python-shell-interpreter " -E \"" buffer-file-name "\""))
-  (local-set-key '[(control return)] 'my-recompile)
-  )
+  (local-set-key '[(control return)] 'my-recompile))
 (add-hook 'python-mode-hook 'my-python-mode-hook)
