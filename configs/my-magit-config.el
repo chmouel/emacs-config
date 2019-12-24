431d37260606f049cd7258ed55943792e418153d
Chmouel Boudjnah <chmouel@chmouel.com>
2019-12-24 09:46:49 +0100

    Fix location of magit forge :D

--- configs/my-magit-config.el
+++ configs/my-magit-config.el
@@ -1,5 +1,6 @@
-(setq force-database-file
+(setq forge-database-file
       (expand-file-name "~/.emacs.d/auto-save-list/forge.database"))
+
 (use-package magit
   :commands (magit-read-repository magit-toplevel)
   :bind (("C-x v v" . my-magit-commit-buffer)
@@ -7,6 +8,11 @@
          ("s-1" . magit-status)
          ("s-2" . magit-show-refs)
          ("C-c g" . magit-status))
+  :bind (:map magit-refs-mode-map
+              ("K" . magit-branch-delete)
+              ("k" . magit-section-backward)
+              ("j" . magit-section-forward)
+              ("." . (lambda () (interactive) (magit-checkout (magit-branch-at-point)))))
   :commands (magit-process-file magit-list-repos-uniquify)
   :config
   (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-topleft-v1)
