912af39be4b054bc52109844235faaefaf141ec8
Chmouel Boudjnah <chmouel@chmouel.com>
2020-02-11 23:33:18 +0100

    Some easy to use functions
    Still need some tweakings

--- configs/programming/go.el
+++ configs/programming/go.el
@@ -3,30 +3,44 @@
   (setq go-playground-ask-file-name t)
   (setq go-playground-basedir "~/tmp/goplay"))
 
+(defun my-go-import-add ()
+  (interactive)
+  (go-import-add nil (read-from-minibuffer "Import: ")))
+
+(defun my-go-next-function()
+  (interactive)
+  (re-search-forward go-func-regexp nil t)
+  )
+
+(defun my-go-prev-function()
+  (interactive)
+  (re-search-backward go-func-regexp nil t)
+  )
+
 (defun my-go-mode-hook ()
   (autoload 'go-test--get-current-test-info "gotest" "" nil)
   (setq gofmt-command "goimports")
 
+  (local-set-key (kbd "C-c a") 'my-go-import-add)
   (local-set-key (kbd "C-c i") 'go-goto-imports)
+  (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+  (local-set-key (kbd "C-M-<up>") 'my-go-prev-function)
+  (local-set-key (kbd "C-M-<down>") 'my-go-next-function)
   (local-set-key (kbd "C-c d") 'godoc-at-point)
   (local-set-key (kbd "C-S-w") (lambda () (interactive) (kill-new (go-test--get-current-test))))
   (local-set-key (kbd "s-t") 'ff-find-other-file)
   (if (and buffer-file-name
            (string-match "_test\\'"
                          (file-name-sans-extension buffer-file-name)))
-      (local-set-key
-       (kbd "C-S-r")
-       '(lambda ()(interactive)
-          (compile (concat "go test -run " (cadr (go-test--get-current-test-info)) "$")))))
+      (progn
+        (local-set-key (kbd "C-S-a") 'go-test-current-project)
+        (local-set-key (kbd "C-S-y") 'go-test-current-file)
+        (local-set-key (kbd "C-S-r") 'go-test-current-test)))
 
   (add-to-list 'company-backends 'company-go)
   (subword-mode)
   (company-mode)
-
-  ;; (use-package go-eldoc :config (go-eldoc-setup))
-  ;; (use-package go-guru :config (go-guru-hl-identifier-mode))
-  ;; (local-set-key (kbd "M-.") 'godef-jump)
 
   (local-set-key '[(control return)] 'my-recompile)
   (local-set-key '[(control meta return)] 'multi-compile-run)
