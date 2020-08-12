deb596cbc226a6e24557377cb2af384ee686e288
Chmouel Boudjnah <chmouel@chmouel.com>
2020-08-12 06:29:58 +0200

    set compile-command

--- configs/programming/progmodes.el
+++ configs/programming/progmodes.el
@@ -2,41 +2,7 @@
 (use-package toggle-quotes)
 (use-package multi-compile)
 
-;;Company mode
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
-
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp)
-
-(use-package lsp-mode
-  :ensure t
-  :commands lsp
-  :custom
-  (lsp-gopls-staticcheck t)
-  (lsp-eldoc-render-all nil)
-  (lsp-gopls-complete-unimported t)
-  (lsp-enable-snippet t)
-  (lsp-enable-links t)
-  ;; (lsp-ui-sideline-delay 0.5)
-  ;; (lsp-ui-doc-max-width 30)
-  ;; (lsp-ui-doc-max-height 15)
-  (lsp-ui-doc-enable t)
-  (lsp-ui-imenu-enable t)
-  (lsp-ui-peek-enable t)
-  (lsp-ui-flycheck-enable t)
-  (lsp-auto-guess-root t)
-  (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
-  :hook
-  ((python-mode) . lsp)
-  ;; ((go-mode) . lsp)
-  )
-
-
+(setq-default compile-command "")
 (defun my-recompile (args)
   (interactive "P")
   (cond
@@ -51,7 +17,7 @@
       (recompile)))
    ((multi-compile--fill-command-list (buffer-file-name))
     (call-interactively 'multi-compile-run))
-   (compile-command (call-interactively 'recompile))
+   ((not (string= "" compile-command)) (call-interactively 'recompile))
    ((call-interactively 'compile))))
 
 (defun my-programming-common-hook()
