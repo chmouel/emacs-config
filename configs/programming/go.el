cca9e9537653a11b76b48205d00c13a206747afc
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-21 23:43:42 +0200

    Back to go without lsp 💁🏽

--- configs/programming/go.el
+++ configs/programming/go.el
@@ -1,54 +1,50 @@
-(use-package gotest
-  :custom
-  ((go-test-verbose t)))
-
 (use-package go-playground
   :init
   (setq go-playground-ask-file-name t)
   (setq go-playground-basedir "~/tmp/goplay"))
 
-(defun my-go-mode-hook ()
-  ;; (setq gofmt-command "goimports")
-  (local-set-key (kbd "C-c a")
-                 (lambda ()
-                   (interactive)
-                   (go-import-add nil (read-from-minibuffer "Import: "))))
-  (local-set-key (kbd "C-M-<up>")
-                 (lambda ()
-                   (interactive)
-                   (re-search-backward go-func-regexp nil t)))
-  (local-set-key (kbd "C-M-<down>")
-                 (lambda ()
-                   (interactive)
-                   (re-search-forward go-func-regexp nil t)))
+(defun my-go-import-add ()
+  (interactive)
+  (go-import-add nil (read-from-minibuffer "Import: ")))
 
-  (local-set-key (kbd "C-S-w")
-                 (lambda ()
-                   (interactive)
-                   (kill-new (go-test--get-current-test))))
-  (when (and
-         buffer-file-name
-         (string-match "_test\\'" (file-name-sans-extension buffer-file-name)))
-    (local-set-key (kbd "C-S-a") 'go-test-current-project)
-    (local-set-key (kbd "C-S-y") 'go-test-current-file)
-    (local-set-key (kbd "C-S-r") 'go-test-current-test))
-
-  (local-set-key (kbd "C-c i") 'go-goto-imports)
-  (local-set-key (kbd "C-c d") 'godoc-at-point)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-  (local-set-key (kbd "s-t") 'ff-find-other-file)
-  (my-programming-common-hook)
-  (subword-mode)
-  (local-set-key (kbd "s-.")  'lsp-describe-thing-at-point)
-  (local-set-key (kbd "s-?")  'lsp-ivy-workspace-symbol)
-
-  (lsp-deferred)
-
-  ;; (local-set-key (kbd "M-.") 'godef-jump)
-  ;; (add-to-list 'company-backends 'company-go)
-  ;; (company-mode)
+(defun my-go-next-function()
+  (interactive)
+  (re-search-forward go-func-regexp nil t)
   )
 
+(defun my-go-prev-function()
+  (interactive)
+  (re-search-backward go-func-regexp nil t)
+  )
+
+(defun my-go-mode-hook ()
+  (autoload 'go-test--get-current-test-info "gotest" "" nil)
+  (setq gofmt-command "goimports")
+
+  (local-set-key (kbd "C-c a") 'my-go-import-add)
+  (local-set-key (kbd "C-c i") 'go-goto-imports)
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+  (local-set-key (kbd "C-M-<up>") 'my-go-prev-function)
+  (local-set-key (kbd "C-M-<down>") 'my-go-next-function)
+  (local-set-key (kbd "C-c d") 'godoc-at-point)
+  (local-set-key (kbd "C-S-w") (lambda () (interactive) (kill-new (go-test--get-current-test))))
+  (local-set-key (kbd "s-t") 'ff-find-other-file)
+  (if (and buffer-file-name
+           (string-match "_test\\'"
+                         (file-name-sans-extension buffer-file-name)))
+      (progn
+        (local-set-key (kbd "C-S-a") 'go-test-current-project)
+        (local-set-key (kbd "C-S-y") 'go-test-current-file)
+        (local-set-key (kbd "C-S-r") 'go-test-current-test)))
+
+  (add-to-list 'company-backends 'company-go)
+  (subword-mode)
+  (company-mode)
+
+  (local-set-key '[(control return)] 'my-recompile)
+  (local-set-key '[(control meta return)] 'multi-compile-run)
+  (my-programming-common-hook))
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (add-hook 'before-save-hook 'gofmt-before-save)
