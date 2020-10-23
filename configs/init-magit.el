e4af86332680bb145bc465afc0bf8a90078d4d7b
Chmouel Boudjnah <chmouel@chmouel.com>
2020-10-23 07:06:09 +0200

    init-magit

--- configs/init-magit.el
+++ configs/init-magit.el
@@ -3,7 +3,9 @@
 (use-package forge
   :bind (:map
          forge-topic-mode-map
-         ("C-c C-v" . my-forge-show-pullreq-diff))
+         ("C-c C-l" . my-forge-show-pullreq-diff)
+         :map magit-mode-map
+         ("C-c C-l" . my-forge-show-pullreq-diff))
   :after magit)
 
 (use-package magit
@@ -11,6 +13,7 @@
   :bind (;; ("C-x v v" . my-magit-commit-buffer)
          ("C-x v -" . magit-pull-from-pushremote)
          ("C-x v =" . my-diff-current-unstaged-file)
+         ("C-x v s-=" . vc-diff)
          ("C-x v P" . magit-push-current-to-pushremote)
          ("<f5>" . magit-status)
          ("C-<f5>" . magit-show-refs)
@@ -19,7 +22,9 @@
               ("K" . magit-branch-delete)
               ("k" . magit-section-backward)
               ("j" . magit-section-forward)
-              ("." . (lambda () (interactive) (magit-checkout (magit-branch-at-point)))))
+              ("." . (lambda ()
+                       (interactive)
+                       (magit-checkout (magit-branch-at-point)))))
   :commands (magit-process-file magit-list-repos-uniquify)
   :hook
   (magit-status-mode-hook my-magit-status-mode-hook)
@@ -32,12 +37,14 @@
 
 (defun my-diff-current-unstaged-file (&optional many)
   (interactive)
-  (if (vc-registered (buffer-file-name))
+  (if (and
+       (buffer-file-name)
+       (vc-registered (buffer-file-name)))
       (if (magit-anything-unstaged-p
            nil `(,(file-name-nondirectory (buffer-file-name))))
           (magit-diff-unstaged '() `(,(magit-file-relative-name)))
         (message "no unstaged changes"))
-    (message "file is not registered in GIT")))
+    (message "buffer is not registered in GIT")))
 
 (defun my-magit-commit-buffer()
   (interactive)
@@ -47,12 +54,15 @@
       (magit-run-git-with-editor
        "commit" (list "-v" (buffer-file-name))))))
 
-(defun my-forge-show-pullreq-diff (n)
-  (interactive (list (forge-read-pullreq "Show pull-request" t)))
+(defun my-forge-show-pullreq-diff ()
+  (interactive)
   (let ((remote-upstream
          (if (member "upstream" (magit-list-remotes))
              "upstream" "origin"))
         (pullreq-number
-         (oref (forge-get-pullreq n) number)))
+         (oref (forge-current-topic) number)))
     (magit-show-commit
      (format "%s/pr/%s" remote-upstream pullreq-number))))
+
+
+(provide 'init-magit)
