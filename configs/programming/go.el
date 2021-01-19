d4fd4e50a099459bcbe510e539ef4155ceb4947e
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-19 12:10:56 +0100

    Switch go and yaml to use-package

--- configs/programming/go.el
+++ configs/programming/go.el
@@ -1,51 +1,51 @@
 (use-package go-playground
   :ensure t
+  :after go-mode
   :init
   (setq go-playground-ask-file-name t)
   (setq go-playground-basedir "~/tmp/goplay"))
 
-(defun my-go-import-add ()
-  (interactive)
-  (go-import-add nil (read-from-minibuffer "Import: ")))
+(use-package go-mode
+  :ensure t
+  :custom
+  (gofmt-command "goimports")
+  :bind (("C-c a" . my-go-import-add)
+         ("C-c i" . go-goto-imports)
+         ("C-c C-r" . go-remove-unused-imports)
+         ("C-M-<up>" . my-go-prev-function)
+         ("C-M-<down>" . my-go-next-function)
+         ("C-c d" . godoc-at-point)
+         ("C-S-w" . (lambda () (interactive) (kill-new (go-test--get-current-test))))
+         ("C-<return>" . my-recompile)
+         ("C-M-<return>" . multi-compile-run)
+         ("C-c t" . ff-find-other-file))
+  :hook ((go-mode . lsp-deferred)
+         (go-mode . subword-mode)
+         (go-mode . my-go-mode-hook)
+         (go-mode . my-programming-common-hook)
+         (before-save . lsp-format-buffer)
+         (before-save . lsp-organize-imports))
+  :config
+  (defun my-go-import-add ()
+    (interactive)
+    (go-import-add nil (read-from-minibuffer "Import: ")))
 
-(defun my-go-next-function()
-  (interactive)
-  (re-search-forward go-func-regexp nil t)
-  )
+  (defun my-go-next-function()
+    (interactive)
+    (re-search-forward go-func-regexp nil t))
 
-(defun my-go-prev-function()
-  (interactive)
-  (re-search-backward go-func-regexp nil t)
-  )
+  (defun my-go-prev-function()
+    (interactive)
+    (re-search-backward go-func-regexp nil t)))
 
 (defun my-go-mode-hook ()
   (autoload 'go-test--get-current-test-info "gotest" "" nil)
   (setq gofmt-command "goimports")
-
-  (local-set-key (kbd "C-c a") 'my-go-import-add)
-  (local-set-key (kbd "C-c i") 'go-goto-imports)
-  (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-  (local-set-key (kbd "C-M-<up>") 'my-go-prev-function)
-  (local-set-key (kbd "C-M-<down>") 'my-go-next-function)
-  (local-set-key (kbd "C-c d") 'godoc-at-point)
-  (local-set-key (kbd "C-S-w") (lambda () (interactive) (kill-new (go-test--get-current-test))))
-  (local-set-key (kbd "C-c t") 'ff-find-other-file)
   (if (and buffer-file-name
            (string-match "_test\\'"
                          (file-name-sans-extension buffer-file-name)))
       (progn
         (local-set-key (kbd "C-S-a") 'go-test-current-project)
         (local-set-key (kbd "C-S-y") 'go-test-current-file)
-        (local-set-key (kbd "C-S-r") 'go-test-current-test)))
+        (local-set-key (kbd "C-S-r") 'go-test-current-test))))
 
-  (add-to-list 'company-backends 'company-go)
-  (subword-mode)
-  (company-mode)
-
-  (local-set-key '[(control return)] 'my-recompile)
-  (local-set-key '[(control meta return)] 'multi-compile-run)
-  (my-programming-common-hook))
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-(add-hook 'before-save-hook 'gofmt-before-save)
--- configs/programming/yaml.el
+++ configs/programming/yaml.el
@@ -1,14 +1,17 @@
-(font-lock-add-keywords
- 'yaml-mode
- '(("\\($(\\(workspaces\\|context\\|params\\)\.[^)]+)\\)" 1 'font-lock-constant-face prepend)
-   ("kind:\s*\\(.*\\)\n" 1 'font-lock-keyword-face prepend)))
-
-(defun my-yaml-mode-hook ()
-  (local-set-key '[(control ret)] 'imenu-list-smart-toggle)
-  (local-set-key '[(control =)] 'whitespace-cleanup)
-  (local-set-key '[(control -)] (lambda () (interactive) (call-interactively 'highlight-indentation-current-column-mode)))
-  (lsp-deferred)
-  (whitespace-mode)
-  (highlight-indentation-mode)
-  (my-programming-common-hook))
-(add-hook 'yaml-mode-hook 'my-yaml-mode-hook)
+(use-package yaml-mode :ensure t
+  :hook
+  (yaml-mode-hook . my-yaml-mode-hookx)
+  :config
+  (font-lock-add-keywords
+   'yaml-mode
+   '(("\\($(\\(workspaces\\|context\\|params\\)\.[^)]+)\\)" 1 'font-lock-constant-face prepend)
+     ("kind:\s*\\(.*\\)\n" 1 'font-lock-keyword-face prepend)))
+  (defun my-yaml-mode-hook ()
+    (local-set-key '[(control ret)] 'imenu-list-smart-toggle)
+    (local-set-key '[(control =)] 'whitespace-cleanup)
+    (local-set-key '[(control -)] (lambda () (interactive) (call-interactively 'highlight-indentation-current-column-mode)))
+    (lsp-deferred)
+    (whitespace-mode)
+    (highlight-indentation-mode)
+    (my-programming-common-hook))
+  )
