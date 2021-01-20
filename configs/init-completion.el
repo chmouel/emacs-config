fc359b9fbef1196ea1bc4937d1b788e13d90e8f0
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-20 19:39:47 +0100

    C-\ as well for completion

--- configs/init-completion.el
+++ configs/init-completion.el
@@ -1,24 +1,7 @@
 ;;; init-completion.el ---                           -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021  Chmouel Boudjnah
-
 ;; Author: Chmouel Boudjnah <chmouel@chmouel.com>
-;; Keywords: 
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
 
 ;; Helm
 (use-package helm :ensure t)
@@ -74,6 +57,7 @@
   :bind (:map icomplete-minibuffer-map
               ("<return>" . icomplete-force-complete-and-exit)              
               ("TAB" . icomplete-force-complete)              
+              ("C-\\" . icomplete-forward-completions)
               ("C-s" . icomplete-forward-completions)
               ("C-n" . icomplete-forward-completions)
               ("C-p" . icomplete-backward-completions)))
