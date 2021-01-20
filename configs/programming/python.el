f7c7f9a5cf3b3ef448586d5b9fcedd1a308bbaba
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-20 02:57:24 +0100

    Move to use-package slowly slowly but surely

--- configs/programming/python.el
+++ configs/programming/python.el
@@ -1,10 +1,17 @@
 ;; (use-package python-docstring :config (python-docstring-install))
-(use-package fill-column-indicator :ensure t)
-(use-package py-isort :ensure t)
-(use-package python-docstring :ensure t)
-(use-package pytest :ensure t)
-(use-package yapfify :ensure t)
-(use-package hungry-delete :ensure t)
+(use-package fill-column-indicator
+  :ensure t
+  :after python-mode)
+(use-package py-isort :ensure t
+  :after python-mode)
+(use-package python-docstring :ensure t
+  :after python-mode)
+(use-package pytest :ensure t
+  :after python-mode)
+(use-package yapfify :ensure t
+  :after python-mode)
+(use-package hungry-delete :ensure t
+  :after python-mode)
 
 (defun my-py-insert-import (arg import)
   (save-excursion
@@ -26,33 +33,36 @@
   (my-py-insert-import arg import)
   (py-isort-buffer))
 
+(use-package python
+  :ensure nil
+  :custom
+  (flycheck-python-flake8-executable python-shell-interpreter)
+  :bind (:map python-mode-map
+              ("C-'" . toggle-quotes)
+              ("C-c t" . ff-find-other-file)
+              ("M-q" . fill-paragraph)
+              ("M-<return>" . compile)
+              ("C-c i" . my-py-import-add)
+              ("C-c \[" . flycheck-previous-error)
+              ("C-c \]" . flycheck-next-error))
+  :hook ((python-mode . yapf-mode)
+         (python-mode . lsp-deferred)
+         (python-mode . flycheck-mode)
+         (python-mode . fci-mode)
+         (python-mode . highlight-indentation-mode)
+         (python-mode . highlight-indentation-mode)
+         (python-mode . my-python-mode-hook)))
+
 (defun my-python-mode-hook()
-  (setq flycheck-python-flake8-executable python-shell-interpreter)
-  (yapf-mode)
-  (fci-mode)
-  (hungry-delete-mode)
-  (python-docstring-mode)
-  ;; (anaconda-mode)
-  ;; (anaconda-eldoc-mode)
-  ;; (company-mode-on)
-  (highlight-indentation-mode)
-  (local-set-key '[(meta q)] 'fill-paragraph)
-  (local-set-key (kbd "C-'") 'toggle-quotes)
-  (local-set-key (kbd "C-c t") 'ff-find-other-file)
-  (if (and buffer-file-name
-           (string-match "\\(^test_\\|_test$\\)"
-                         (file-name-base (file-name-sans-extension buffer-file-name))))
+  (if (and
+       buffer-file-name
+       (string-match
+        "\\(^test_\\|_test$\\)"
+        (file-name-base (file-name-sans-extension buffer-file-name))))
       (progn
         (local-set-key '[(control return)] 'pytest-one)
         (local-set-key (kbd "C-S-r") 'pytest-one))
     (local-set-key '[(control return)] 'my-recompile))
-
-  (local-set-key '[(meta return)] 'compile)
-  (local-set-key '[(control c)(i)] 'my-py-import-add)
-  (local-set-key '[(control c)(\[)] 'flycheck-previous-error)
-  (local-set-key '[(control c)(\])] 'flycheck-next-error)
-  (flycheck-mode)
-  (lsp-deferred)
-  (setq-local compile-command (concat python-shell-interpreter " -E \"" buffer-file-name "\"")))
-
-(add-hook 'python-mode-hook 'my-python-mode-hook)
+  (setq-local
+   compile-command
+   (concat python-shell-interpreter " -E \"" buffer-file-name "\"")))
