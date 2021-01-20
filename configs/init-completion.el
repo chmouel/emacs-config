e76d2f4f7c2bd20f04388de962c6fc5ca3d9bcad
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-20 04:17:37 +0100

    Trying selectrum
    looks nice so far

--- configs/init-completion.el
+++ configs/init-completion.el
@@ -20,56 +20,75 @@
 
 ;;; Commentary:
 
-;;Helm
-(use-package helm
-  :ensure t
-  :custom
-  (smex-save-file
-   (locate-user-emacs-file
-    "auto-save-list/smex-items")))
+;; Helm
+(use-package helm :ensure t)
 
+;; IDO
 (use-package ido
   :defer t
   :bind
-  (("C-x C-f" . ido-find-file)
-   :map ido-common-completion-map
-   ("C-\\" . ido-next-match)
-   :map ido-completion-map
-   (" "   . ido-next-match)))
+  (:map ido-common-completion-map
+        ("C-\\" . ido-next-match)
+        :map ido-completion-map
+        (" "   . ido-next-match)))
 
-(use-package ivy-prescient
+(use-package prescient
   :ensure t
+  :config
+  (prescient-persist-mode t)  
   :custom
   (prescient-sort-length-enable nil)
-  (prescient-filter-method '(literal fuzzy anchored))
-  :after counsel
-  :config
-  (prescient-persist-mode)
-  (ivy-prescient-mode 1))
+  (prescient-filter-method '(literal fuzzy anchored)))
 
-;; Counsel
-(use-package counsel
+(use-package marginalia 
   :ensure t
-  :after ivy
+  :init
+  (marginalia-mode t)
+  ;; When using Selectrum, ensure that Selectrum is refreshed when cycling annotations.
+  (advice-add #'marginalia-cycle :after
+              (lambda () (when (bound-and-true-p selectrum-mode) (selectrum-exhibit))))
+  (setq marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light))
+  :bind (:map minibuffer-local-completion-map
+              ("C-i" . marginalia-cycle-annotators)))
+
+(use-package selectrum
+  :ensure t
   :custom
-  (counsel-find-file-ignore-regexp
-   (regexp-opt completion-ignored-extensions))
-  :bind (("C-h f"   . counsel-describe-function)
-         ("C-x b"   . counsel-switch-buffer)
-         ("C-\\"    . counsel-switch-buffer)
-         ("C-x r b" . counsel-bookmark)
-         ("M-x"     . counsel-M-x)))
+  (selectrum-count-style 'nil)
+  (selectrum-extend-current-candidate-highlight t)
+  :config
+  (selectrum-mode))
+
+(use-package selectrum-prescient
+  :ensure t
+  :after selectrum
+  :config
+  (selectrum-prescient-mode t))
+
+(use-package consult
+  :ensure t
+  :bind (("M-s o" . consult-outline)
+         ("M-s i" . consult-imenu)
+         ("C-x r b" . consult-bookmark)
+         ("M-y" . consult-yank-pop)))
+
+;;; Ido Vertical mode
+(use-package ido-vertical-mode
+  :ensure t
+  :custom
+  (ido-max-prospects 10)
+  :config
+  (ido-vertical-mode 1))
 
 ;; Ivy
 (use-package ivy
   :ensure t
+  :disabled
   :config
   (ivy-mode)
   :bind
   (:map ivy-minibuffer-map
-        ("C-\\" . ivy-next-line)
-        ;; ("<tab>" . ivy-partial)
-        )
+        ("C-\\" . ivy-next-line))
   :custom
   (counsel-switch-buffer-preview-virtual-buffers nil)
   (ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
@@ -78,6 +97,7 @@
   (ivy-use-virtual-buffers t))
 
 (use-package all-the-icons-ivy
+  :disabled
   :ensure t
   :after (all-the-icons ivy)
   :custom
@@ -93,13 +113,26 @@
      counsel-recentf))
   :config (all-the-icons-ivy-setup))
 
-;;;Ido Vertical mode
-(use-package ido-vertical-mode
+(use-package ivy-prescient
   :ensure t
-  :custom
-  (ido-max-prospects 10)
+  :disabled
+  :after counsel
   :config
-  (ido-vertical-mode 1))
+  (ivy-prescient-mode 1))
+
+;; Counsel
+(use-package counsel
+  :ensure t
+  :disabled
+  :after ivy
+  :custom
+  (counsel-find-file-ignore-regexp
+   (regexp-opt completion-ignored-extensions))
+  :bind (("C-h f"   . counsel-describe-function)
+         ("C-x b"   . counsel-switch-buffer)
+         ("C-\\"    . counsel-switch-buffer)
+         ("C-x r b" . counsel-bookmark)
+         ("M-x"     . counsel-M-x)))
 
 ;;;; Disabled
 ;; flx-ido - advanced flex matching for ido
