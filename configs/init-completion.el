ceb9c7dc57f44ffb98ffafc2c7e9094780bab2e6
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-21 22:13:02 +0100

    Back to square one with ido!

--- configs/init-completion.el
+++ configs/init-completion.el
@@ -28,7 +28,17 @@
         :map ido-completion-map
         (" "   . ido-next-match)))
 
-;;; Ido Vertical mode
+;; Flex IDO
+;; flx-ido - advanced flex matching for ido
+(use-package flx-ido
+  :ensure t
+  :custom
+  (ido-enable-flex-matching t)
+  (ido-use-faces nil)
+  :config
+  (flx-ido-mode 1))
+
+;; Ido Vertical mode
 (use-package ido-vertical-mode
   :ensure t
   :custom
@@ -36,8 +46,8 @@
   :config
   (ido-vertical-mode 1))
 
+;; Prettiness
 (use-package marginalia
-  :disabled
   :ensure t
   :config
   (setq marginalia-annotators
@@ -45,10 +55,65 @@
           marginalia-annotators-light))
   (marginalia-mode 1))
 
+                                        ; Use flex for completion-styles
 (use-package minibuffer
   :custom
   (completion-styles
    '(basic partial-completion flex)))
+
+
+;; Ivy
+(use-package ivy
+  :ensure t
+  :bind
+  (:map ivy-minibuffer-map
+        ("C-\\" . ivy-next-line))
+  :custom
+  (counsel-switch-buffer-preview-virtual-buffers nil)
+  (ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  (ivy-extra-directories nil)
+  (ivy-count-format "")
+  (ivy-use-virtual-buffers t))
+
+(use-package all-the-icons-ivy
+  :ensure t
+  :after (all-the-icons ivy)
+  :custom
+  (all-the-icons-spacer " ")
+  (all-the-icons-ivy-file-commands
+   '(counsel-dired-jump
+     counsel-find-file
+     counsel-file-jump
+     counsel-find-library
+     counsel-git
+     counsel-projectile-find-dir
+     counsel-projectile-find-file
+     counsel-recentf))
+  :config (all-the-icons-ivy-setup))
+
+;; Counsel
+(use-package counsel
+  :ensure t
+  :after ivy
+  :custom
+  (setq
+   counsel-find-file-ignore-regexp
+   (regexp-opt
+    (append completion-ignored-extensions (quote (".#")))))
+  ;; (regexp-opt completion-ignored-extensions))
+  :bind (("C-h f"   . counsel-describe-function)
+         ("C-x b"   . counsel-switch-buffer)
+         ("C-\\"    . counsel-switch-buffer)
+         ("C-x r b" . counsel-bookmark)
+         ("M-x"     . counsel-M-x)))
+
+;;; DISABLED
+(use-package ivy-prescient
+  :disabled
+  :ensure t
+  :after counsel
+  :config
+  (ivy-prescient-mode 1))
 
 (use-package icomplete
   :disabled
@@ -81,6 +146,7 @@
 
 ;;  Disabled
 (use-package prescient
+  :disabled
   :ensure t
   :config
   (prescient-persist-mode t)  
@@ -88,6 +154,7 @@
   (prescient-sort-length-enable nil)
   (prescient-filter-method '(anchored regexp fuzzy)))
 
+;;
 (use-package selectrum
   :ensure t
   :disabled
@@ -98,75 +165,13 @@
   (selectrum-mode))
 
 (use-package selectrum-prescient
+  :disabled
   :ensure t
   :after selectrum
-  :disabled
   :config
   (selectrum-prescient-mode t))
 
-;; Ivy
-(use-package ivy
-  :ensure t
-  :config
-  (ivy-mode)
-  :bind
-  (:map ivy-minibuffer-map
-        ("C-\\" . ivy-next-line))
-  :custom
-  (counsel-switch-buffer-preview-virtual-buffers nil)
-  (ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-  (ivy-extra-directories nil)
-  (ivy-count-format "")
-  (ivy-use-virtual-buffers t))
-
-(use-package all-the-icons-ivy
-  :ensure t
-  :after (all-the-icons ivy)
-  :custom
-  (all-the-icons-spacer " ")
-  (all-the-icons-ivy-file-commands
-   '(counsel-dired-jump
-     counsel-find-file
-     counsel-file-jump
-     counsel-find-library
-     counsel-git
-     counsel-projectile-find-dir
-     counsel-projectile-find-file
-     counsel-recentf))
-  :config (all-the-icons-ivy-setup))
-
-(use-package ivy-prescient
-  :ensure t
-  :after counsel
-  :config
-  (ivy-prescient-mode 1))
-
-;; Counsel
-(use-package counsel
-  :ensure t
-  :after ivy
-  :custom
-  (setq
-   counsel-find-file-ignore-regexp
-   (regexp-opt
-    (append completion-ignored-extensions (quote (".#")))))
-  ;; (regexp-opt completion-ignored-extensions))
-  :bind (("C-h f"   . counsel-describe-function)
-         ("C-x C-f" . counsel-find-file)
-         ("C-x b"   . counsel-switch-buffer)
-         ("C-\\"    . counsel-switch-buffer)
-         ("C-x r b" . counsel-bookmark)
-         ("M-x"     . counsel-M-x)))
-
 ;;;; Disabled
-;; flx-ido - advanced flex matching for ido
-(use-package flx-ido
-  :ensure t
-  :custom
-  (ido-enable-flex-matching t)
-  (ido-use-faces nil)
-  :config
-  (flx-ido-mode 1))
 
 (use-package amx
   :disabled
