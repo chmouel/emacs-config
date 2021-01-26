505ca8706e1ee49d864fe098fb75e0e44fb94918
Chmouel Boudjnah <chmouel@chmouel.com>
2021-01-26 18:27:03 +0100

    Redo yaml properly

--- configs/programming/yaml.el
+++ configs/programming/yaml.el
@@ -1,17 +1,18 @@
 (use-package yaml-mode :ensure t
   :hook
-  (yaml-mode-hook . my-yaml-mode-hookx)
+  (yaml-mode-hook . my-yaml-mode-hook)
+  (yaml-mode-hook . whitespace-mode)
+  (yaml-mode-hook . highlight-indentation-mode)
+  (yaml-mode-hook . my-programming-common-hook)
+  (yaml-mode-hook . lsp-deferred)
   :config
   (font-lock-add-keywords
    'yaml-mode
    '(("\\($(\\(workspaces\\|context\\|params\\)\.[^)]+)\\)" 1 'font-lock-constant-face prepend)
      ("kind:\s*\\(.*\\)\n" 1 'font-lock-keyword-face prepend)))
-  (defun my-yaml-mode-hook ()
-    (local-set-key '[(control ret)] 'imenu-list-smart-toggle)
-    (local-set-key '[(control =)] 'whitespace-cleanup)
-    (local-set-key '[(control -)] (lambda () (interactive) (call-interactively 'highlight-indentation-current-column-mode)))
-    (lsp-deferred)
-    (whitespace-mode)
-    (highlight-indentation-mode)
-    (my-programming-common-hook))
-  )
+  :bind
+  (:map yaml-mode-map
+        ("C-<ret>" . imenu-list-smart-toggle)
+        ("C--" . (lambda () (interactive) (call-interactively 'highlight-indentation-current-column-mode)))
+        ("C-=" . whitespace-cleanup)))
+
