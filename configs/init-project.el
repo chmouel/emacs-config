4150d23e77785e4ba0f735c48242d4d20cac2b83
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-03 11:20:18 +0100

    my-project magit

--- configs/init-project.el
+++ configs/init-project.el
@@ -1,6 +1,10 @@
 (use-package project
   :ensure t
   :bind
-  ("C-x p v" . project-vc-dir)
+  ("C-x p v" . my-project-magit)
+  :config
+  (defun my-project-magit ()
+    (interactive)
+    (magit-status (project-root (project-current t))))
   :custom
   (project-list-file (concat user-emacs-directory "/auto-save-list/project-list.file")))
