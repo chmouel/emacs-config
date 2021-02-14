a18e4fab7960fe2b9fdde680d37b5ad9f0ee838f
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-14 15:15:45 +0100

    define zap to isearch

--- configs/my-functions.el
+++ configs/my-functions.el
@@ -138,3 +138,33 @@
 
     (set-frame-size (selected-frame) (/ WIDTH 2) HEIGHT t)
     (set-frame-position (selected-frame) x 0)))
+
+;; From Sacha's emacs
+(defun zap-to-isearch (rbeg rend)
+  "Kill the region between the mark and the closest portion of
+      the isearch match string. The behaviour is meant to be analogous
+      to zap-to-char; let's call it zap-to-isearch. The deleted region
+      does not include the isearch word. This is meant to be bound only
+      in isearch mode.  The point of this function is that oftentimes
+      you want to delete some portion of text, one end of which happens
+      to be an active isearch word. The observation to make is that if
+      you use isearch a lot to move the cursor around (as you should,
+      it is much more efficient than using the arrows), it happens a
+      lot that you could just delete the active region between the mark
+      and the point, not include the isearch word."
+  (interactive "r")
+  (when (not mark-active)
+    (error "Mark is not active"))
+  (let* ((isearch-bounds (list isearch-other-end (point)))
+         (ismin (apply 'min isearch-bounds))
+         (ismax (apply 'max isearch-bounds))
+         )
+    (if (< (mark) ismin)
+        (kill-region (mark) ismin)
+      (if (> (mark) ismax)
+          (kill-region ismax (mark))
+        (error "Internal error in isearch kill function.")))
+    (isearch-exit)
+    ))
+
+(define-key isearch-mode-map [(meta z)] 'zap-to-isearch)
