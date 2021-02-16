91bb187d3550dcbd772eddf2829fb511ba4fb0aa
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-16 11:51:09 +0100

    Big move to init-emacs for default-mode and keybinding to use-package

--- configs/init-emacs.el
+++ configs/init-emacs.el
@@ -1,3 +1,78 @@
+(use-package emacs
+  :bind
+  ("C-<next>" . end-of-buffer)
+  ("C-<prior>" . beginning-of-buffer)
+  ("C-c \]" . next-error)
+  ("C-c \[" . previous-error)
+
+  ("C-x C-=" . ediff-buffers)
+  ;; kill-current-buffer works weirdly for me, it switch to another buffer i am
+  ;; not expected with and i have been using that keybining/lambda to a very long time
+  ("C-x C-k" . (lambda ()
+                 (interactive)(kill-buffer (current-buffer))))
+
+  ("C-x f" . find-file-at-point)
+  ("M-q" . fill-region)
+
+  ("C-c <up>" . backward-paragraph)
+  ("C-c <down>" . forward-paragraph)
+
+  ("C-x <left>" . indent-rigidly-left)
+  ("C-x <right>" . indent-rigidly-right)
+
+  ( "C-2" . split-window-vertically)
+
+  ;; Redefine {for/back}ward paragraphs to just 4 lines
+  ("C-<down>" . 
+   (lambda ()
+     (interactive)
+     (ignore-errors (next-logical-line 5))))
+
+  ("C-<up>" .
+   (lambda ()
+     (interactive)
+     (ignore-errors (previous-logical-line 5))))
+
+  ;; Go to GNUS
+  ("<f2>" .
+   (lambda ()
+     (interactive)
+     (let ((tab-index (or (tab-bar--tab-index-by-name "*Group*") -1)))
+       (if (= tab-index -1)
+           (progn
+             (tab-bar-select-tab tab-index)
+             (gnus)))
+       (tab-bar-select-tab (1+ tab-index)))))  
+
+
+  :hook
+  (after-save . executable-make-buffer-file-executable-if-script-p)
+  (before-save . my-delete-trailing-whitespace)
+  (after-init . windmove-default-keybindings)
+  
+  :config
+  (defvar my-delete-trailing-whitespace-enabled t)
+  (defun my-delete-trailing-whitespace ()
+    (if my-delete-trailing-whitespace-enabled (delete-trailing-whitespace)))
+  
+  (when (not window-system)
+    (define-key function-key-map "\eO1;5A"    [C-up])
+    (define-key function-key-map "\eO1;5B"  [C-down])
+    (define-key function-key-map "\eO1;5C" [C-right])
+    (define-key function-key-map "\eO1;5D"  [C-left]))  
+
+  (global-unset-key "\C-x\C-z")
+  (global-unset-key "\C-z")
+  
+  (fset 'yes-or-no-p 'y-or-n-p)  
+  :custom
+  ;; Bookmarks
+  (url-history-file (locate-user-emacs-file "auto-save-list/url-history"))
+  (package-quickstart-file (locate-user-emacs-file "auto-save-list/package-quickstart.el"))
+  (bookmark-default-file (locate-user-emacs-file "auto-save-list/bookmarks.bmk"))
+  (save-place-file (locate-user-emacs-file "auto-save-list/emacs-places.el"))
+  (nsm-settings-file (locate-user-emacs-file "auto-save-list/network-security.data")))
+
 ;; Save Places
 (use-package saveplace :config (setq-default save-place t))
 
@@ -76,11 +151,10 @@ With a prefix argument P, isearch for the symbol at point."
 
 ;; IBUFFER
 (use-package ibuffer
-  :init
-  (add-hook 'ibuffer-mode-hook
-	        '(lambda ()
-	           (ibuffer-auto-mode 1)
-	           (ibuffer-filter-by-visiting-file nil)))
+  :hook
+  (ibuffer-mode . (lambda ()
+	                (ibuffer-auto-mode 1)
+	                (ibuffer-filter-by-visiting-file nil)))
   :custom (ibuffer-saved-filter-groups
            (quote (
                    ("default"
@@ -93,8 +167,8 @@ With a prefix argument P, isearch for the symbol at point."
 
 ;; Shell Mode
 (use-package shell
-  :config
-  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+  :hook
+  (shell-mode . ansi-color-for-comint-mode-on)
   :custom
   (
    comint-prompt-read-only t
@@ -112,9 +186,8 @@ With a prefix argument P, isearch for the symbol at point."
                                     (switch-to-buffer (other-buffer nil)))))))
 ;;  Flyspell mode
 (use-package vc
-  :defer t
-  :config
-  (add-hook 'log-edit-mode-hook 'flyspell-mode))
+  :hook
+  (log-edit-mode . flyspell-mode))
 
 ;;  Define j/k for scroll up/down on view-mode and derived.
 (defun my-view-vi-keys-map (x-map)
@@ -146,8 +219,8 @@ With a prefix argument P, isearch for the symbol at point."
       (toggle-read-only))))
 
 (use-package server
-  :config
-  (add-hook 'after-init-hook 'server-mode))
+  :hook
+  (after-init . server-mode))
                                         ;
 (use-package eldoc)
 (use-package subword)
@@ -229,19 +302,4 @@ mouse-3: go to end"))))
    eshell-prefer-lisp-functions nil
    eshell-destroy-buffer-when-process-dies t))
 
-(use-package emacs
-  :ensure nil
-  :config
-  (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
-  (add-hook 'before-save-hook 'my-delete-trailing-whitespace)
-  (defvar my-delete-trailing-whitespace-enabled 't)
-  (defun my-delete-trailing-whitespace ()
-    (if my-delete-trailing-whitespace-enabled (delete-trailing-whitespace)))
-  :custom
-  ;; Bookmarks
-  (url-history-file (locate-user-emacs-file "auto-save-list/url-history"))
-  (package-quickstart-file (locate-user-emacs-file "auto-save-list/package-quickstart.el"))
-  (bookmark-default-file (locate-user-emacs-file "auto-save-list/bookmarks.bmk"))
-  (save-place-file (locate-user-emacs-file "auto-save-list/emacs-places.el"))
-  (nsm-settings-file (locate-user-emacs-file "auto-save-list/network-security.data")))
-
+(provide 'init-emacs)
