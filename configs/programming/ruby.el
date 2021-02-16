cc9a215742926a6147b21f14e43f8aae22c16646
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-16 11:13:58 +0100

    use-package ruby

--- configs/programming/ruby.el
+++ configs/programming/ruby.el
@@ -1,23 +1,26 @@
-(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
-
-(defun my-ruby-mode-hook()
-  (inf-ruby-keys)
-  (set (make-local-variable 'my-compile-command)
-       (concat "ruby -c \"" buffer-file-name "\""))
-  (or (file-exists-p "makefile") (file-exists-p "Makefile")
-      (set (make-local-variable 'my-compile-run-command)
-           (concat "ruby \"" buffer-file-name "\"")))
-  (local-set-key '[(control meta b)] 'bookmark-jump)
-  (my-programming-common-hook))
-
-(setq ruby-program-name "irb --inf-ruby-mode --simple-prompt"
-      interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist)
-      inferior-ruby-first-prompt-pattern ">> "
-      inferior-ruby-prompt-pattern ">> " )
+(use-package inf-ruby
+  :hook
+  (ruby-mode . inf-ruby-keys)
+  :after ruby-mode
+  :commands (run-ruby inf-ruby-keys))
 
 (use-package ruby-mode
+  :mode "\\.rb\\'"
+  :interpreter "ruby"  
+  :custom
+  (ruby-program-name "irb --inf-ruby-mode --simple-prompt")
+  (interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
+  (inferior-ruby-first-prompt-pattern ">> ")
+  (inferior-ruby-prompt-pattern ">> ")  
+  :hook
+  (ruby-mode . my-ruby-mode-hook)
+  (ruby-mode . my-programming-common-hook)
+  :bind
+  (:map ruby-mode-map ("C-M-b" . bookmark-jump))
   :config
-  (setq auto-mode-alist (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
-  (add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
-  (use-package inf-ruby
-    :commands (run-ruby inf-ruby-keys)))
+  (defun my-ruby-mode-hook()
+    (set (make-local-variable 'my-compile-command)
+         (concat "ruby -c \"" buffer-file-name "\""))
+    (or (file-exists-p "makefile") (file-exists-p "Makefile")
+        (set (make-local-variable 'my-compile-run-command)
+             (concat "ruby \"" buffer-file-name "\"")))))
