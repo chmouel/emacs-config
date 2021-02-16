de77d9a4cf5f4d72c9f4ee7e08188be0a7e0705e
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-16 12:10:36 +0100

    buggy, let's forget about it

--- configs/init-project.el
+++ configs/init-project.el
@@ -1,14 +1,47 @@
+(defun my-projectile-and-dired()
+  (interactive)
+  (let ((projectile-switch-project-action 'projectile-dired))
+    (projectile-switch-project)))
+
+(defun my-projectile-mode-line ()
+  (let ((project-name (projectile-project-name)))
+    (if (not (string= project-name "-"))
+        (format
+         " %s"
+         (propertize
+          project-name 'face '(:foreground "Light Blue" :weight bold))))))
+
+
+(defun my-projectile-and-find-file()
+  (interactive)
+  (let ((projectile-switch-project-action 'my-projectile-find-file))
+    (projectile-switch-project)))
+
+(defun my-projectile-and-ripgrep()
+  (interactive)
+  (let ((consult-preview-key 'any)
+        (projectile-switch-project-action 'consult-ripgrep))
+    (projectile-switch-project)))
+
+(defun my-projectile-find-file()
+  (interactive)
+  (let* ((project-root (projectile-ensure-project (projectile-project-root)))
+         (files
+          (if (boundp 'my-project-ignore-files)
+              (--filter (not (string-match my-project-ignore-files it))
+                        (projectile-project-files project-root))
+            (projectile-project-files project-root)))
+         (file (completing-read "Find file: " (sort files #'string<))))
+    (when file
+      (funcall 'find-file (expand-file-name file project-root))
+      (run-hooks 'projectile-find-file-hook))))
+
 (use-package project
   :ensure t
   :bind
   ("C-c u" . project-find-regexp)
-  ("C-x p v" . my-project-magit)
-  :config
-  (defun my-project-magit ()
-    (interactive)
-    (magit-status (project-root (project-current t))))
   :custom
-  (project-list-file (concat user-emacs-directory "/auto-save-list/project-list.file")))
+  (project-list-file (locate-user-emacs-file "auto-save-list/project-list.file")))
 
 ;; projectile
 (use-package projectile
@@ -35,42 +68,5 @@
          ("C-c S-<return>" . projectile-commander)
          ("C-c <return>" . projectile-switch-open-project)
          ("C-c p" . my-projectile-and-dired)))
-
-(defun my-projectile-mode-line ()
-  (let ((project-name (projectile-project-name)))
-    (if (not (string= project-name "-"))
-        (format
-         " %s"
-         (propertize
-          project-name 'face '(:foreground "Light Blue" :weight bold))))))
-
-(defun my-projectile-and-dired()
-  (interactive)
-  (let ((projectile-switch-project-action 'projectile-dired))
-    (projectile-switch-project)))
-
-(defun my-projectile-and-find-file()
-  (interactive)
-  (let ((projectile-switch-project-action 'my-projectile-find-file))
-    (projectile-switch-project)))
-
-(defun my-projectile-and-ripgrep()
-  (interactive)
-  (let ((consult-preview-key 'any)
-        (projectile-switch-project-action 'consult-ripgrep))
-    (projectile-switch-project)))
-
-(defun my-projectile-find-file()
-  (interactive)
-  (let* ((project-root (projectile-ensure-project (projectile-project-root)))
-         (files
-          (if (boundp 'my-project-ignore-files)
-              (--filter (not (string-match my-project-ignore-files it))
-                        (projectile-project-files project-root))
-            (projectile-project-files project-root)))
-         (file (completing-read "Find file: " (sort files #'string<))))
-    (when file
-      (funcall 'find-file (expand-file-name file project-root))
-      (run-hooks 'projectile-find-file-hook))))
 
 (provide 'init-project)
