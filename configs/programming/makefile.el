099aadc413f6737098960f6d7e2dda7f33f5c837
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-16 11:13:38 +0100

    use-package makefile-mode

--- configs/programming/makefile.el
+++ configs/programming/makefile.el
@@ -1,5 +1,8 @@
-(defun my-makefile-mode-hook ()
-  (my-programming-common-hook)
-  (local-set-key '[(control =)] 'whitespace-cleanup)
-  (whitespace-mode))
-(add-hook 'makefile-mode-hook 'my-makefile-mode-hook)
+(use-package make-mode
+  :bind
+  (:map makefile-mode-map
+        ("C-=" . whitespace-cleanup))
+  :hook
+  (makefile-mode . my-makefile-mode-hook)
+  (makefile-mode . my-programming-common-hook)
+  (makefile-mode . whitespace-mode))
