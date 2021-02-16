b170d5998699a0fd7daef355d7a5bfa4f4768304
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-16 11:14:03 +0100

    use-package sh-script

--- configs/programming/shell.el
+++ configs/programming/shell.el
@@ -1,11 +1,10 @@
-(defvar shfmt-command "shfmt")
-(reformatter-define shell-formatter
-  :program shfmt-command
-  :args '("-i" "2")
-  :lighter 'SHFmt)
+(use-package sh-script
+  :hook
+  (sh-mode . my-programming-common-hook)
+  (sh-mode . my-sh-mode-hook)
+  :config
+  (defun my-sh-mode-hook()
+    (set (make-local-variable 'my-compile-command) (concat "bash -n \"" buffer-file-name "\""))
+    (set (make-local-variable 'my-compile-run-command) (concat "bash \"" buffer-file-name "\""))))
 
-(defun my-sh-mode-hook()
-  (set (make-local-variable 'my-compile-command) (concat "bash -n \"" buffer-file-name "\""))
-  (set (make-local-variable 'my-compile-run-command) (concat "bash \"" buffer-file-name "\""))
-  (my-programming-common-hook))
 (add-hook 'sh-mode-hook 'my-sh-mode-hook)
