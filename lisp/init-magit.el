6be227828dd3e8ae6fc61bc9c20f8ea47c382cad
Chmouel Boudjnah <chmouel@chmouel.com>
2021-02-25 22:23:29 +0100

    Switcheroos stuff around!

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -23,38 +23,38 @@
   (transient-levels-file (locate-user-emacs-file "auto-save-list/transient.levels.el"))
   (magit-display-buffer-function 'magit-display-buffer-fullframe-status-topleft-v1)
   :config
+  (defun my-diff-current-unstaged-file (&optional many)
+	(interactive)
+	(if (and
+		 (buffer-file-name)
+		 (vc-registered (buffer-file-name)))
+		(if (magit-anything-unstaged-p
+			 nil `(,(file-name-nondirectory (buffer-file-name))))
+			(magit-diff-unstaged '() `(,(magit-file-relative-name)))
+          (message "no unstaged changes"))
+      (message "buffer is not registered in GIT")))
+
+  (defun my-magit-commit-buffer()
+	(interactive)
+	(if (not (magit-anything-modified-p nil (list (buffer-file-name))))
+		(message "Fileset is up-to-date")
+      (let ((default-directory (magit-toplevel)))
+		(magit-run-git-with-editor
+		 "commit" (list "-v" (buffer-file-name))))))
+
+  (defun my-forge-show-pullreq-diff ()
+	(interactive)
+	(let ((remote-upstream
+           (if (member "upstream" (magit-list-remotes))
+               "upstream" "origin"))
+          (pullreq-number
+           (oref (forge-current-topic) number)))
+      (magit-show-commit
+       (format "%s/pr/%s" remote-upstream pullreq-number))))  
   (defun my-magit-status-mode-hook ()
     (local-set-key '[(control =)] 'magit-commit-create))
   (global-git-commit-mode))
 
-(defun my-diff-current-unstaged-file (&optional many)
-  (interactive)
-  (if (and
-       (buffer-file-name)
-       (vc-registered (buffer-file-name)))
-      (if (magit-anything-unstaged-p
-           nil `(,(file-name-nondirectory (buffer-file-name))))
-          (magit-diff-unstaged '() `(,(magit-file-relative-name)))
-        (message "no unstaged changes"))
-    (message "buffer is not registered in GIT")))
-
-(defun my-magit-commit-buffer()
-  (interactive)
-  (if (not (magit-anything-modified-p nil (list (buffer-file-name))))
-      (message "Fileset is up-to-date")
-    (let ((default-directory (magit-toplevel)))
-      (magit-run-git-with-editor
-       "commit" (list "-v" (buffer-file-name))))))
-
-(defun my-forge-show-pullreq-diff ()
-  (interactive)
-  (let ((remote-upstream
-         (if (member "upstream" (magit-list-remotes))
-             "upstream" "origin"))
-        (pullreq-number
-         (oref (forge-current-topic) number)))
-    (magit-show-commit
-     (format "%s/pr/%s" remote-upstream pullreq-number))))
 
 
 (provide 'init-magit)
