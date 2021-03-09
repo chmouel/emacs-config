789772ea2dfb652d24cac3b8af1326b590b09b77
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-09 14:42:59 +0100

    modify themes we want

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -12,7 +12,7 @@
 
 (use-package rand-theme
   :custom
-  (rand-theme-wanted '(doom-laserwave doom-oceanic-next doom-material doom-horizon doom-palenight doom-vibrant doom-snazzy doom-dracula doom-outrun-electric))
+  (rand-theme-wanted '(doom-laserwave ubuntu doom-oceanic-next doom-material doom-horizon doom-palenight doom-vibrant doom-snazzy doom-dracula doom-outrun-electric))
   :init
   (rand-theme))
 
