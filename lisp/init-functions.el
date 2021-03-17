d0746ba217fc5af9d8d082c72c15e5566eab42da
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-17 11:50:11 +0100

    add forward-or-backward-sexp

--- lisp/init-functions.el
+++ lisp/init-functions.el
@@ -152,4 +152,15 @@
       (emacs-lisp-mode)
       (font-lock-ensure))))
 
-(provide 'init-functions)
+;;https://www.emacswiki.org/emacs/NavigatingParentheses
+(defun forward-or-backward-sexp (&optional arg)
+  "Go to the matching parenthesis character if one is adjacent to point."
+  (interactive "^p")
+  (cond ((looking-at "\\s(") (forward-sexp arg))
+        ((looking-back "\\s)" 1) (backward-sexp arg))
+        ;; Now, try to succeed from inside of a bracket
+        ((looking-at "\\s)") (forward-char) (backward-sexp arg))
+        ((looking-back "\\s(" 1) (backward-char) (forward-sexp arg))))
+(global-set-key (read-kbd-macro "C-c /") 'forward-or-backward-sexp)
+
+ (provide 'init-functions)
