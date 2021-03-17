124f136bc6ad957ce3c644d88094d46d6f248e9c
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-17 14:45:18 +0100

    Make sure to cycle in between when we got to the bottom of the selection

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -109,25 +109,32 @@
   :hook (company-mode . company-box-mode))
 
 (use-package selectrum
+  :hook (after-init . selectrum-mode)
   :preface (declare-function chmou-selectrum-insert-or-submit-current-candidate nil)
   :init
   (defun chmou-selectrum-classic-find-file-completion ()
     "This tries to mimic the classic find file completion
 
-If multiple matches, go to next candidates.
+If multiple matches, go to next candidates, and rotate if we are at the bottom.
 If only one match, select it.
 If current selection is a directory insert it in the current completion."
     (interactive)
     (progn
-      (let* ((index (selectrum--index-for-arg nil))
-             (selection (selectrum--get-candidate index))))
-      (cond
-       ((string-suffix-p "/" selection)
-        (selectrum-insert-current-candidate))
-       ((> (length (selectrum-get-current-candidates)) 1)
-        (selectrum-next-candidate))
-       ((= (length (selectrum-get-current-candidates)) 1)
-        (selectrum-select-current-candidate)))))
+      (let* ((index
+              (if (and selectrum--current-candidate-index
+                       (not (< selectrum--current-candidate-index 0)))
+                  selectrum--current-candidate-index 0))
+             (matchesn (length (selectrum-get-current-candidates)))
+             (selection (selectrum--get-candidate index)))
+        (cond
+         ((string-suffix-p "/" selection)
+          (selectrum-insert-current-candidate))
+         ((> (length (selectrum-get-current-candidates)) 1)
+          (if (= (+ 1 index) matchesn) ;; Rotate if we are at the end
+              (selectrum-next-candidate (-(- matchesn 1)))
+            (selectrum-next-candidate)))
+         ((= (length (selectrum-get-current-candidates)) 1)
+          (selectrum-select-current-candidate))))))
   :ensure t
   :custom-face
   (selectrum-current-candidate ((t
@@ -136,7 +143,7 @@ If current selection is a directory insert it in the current completion."
   :bind
   (:map selectrum-minibuffer-map
         ("C-s" . selectrum-next-candidate)
-        ("TAB" . selectrum-insert-or-submit-current-candidate)        
+        ("TAB" . chmou-selectrum-classic-find-file-completion)        
         ("C-RET" . selectrum-submit-exact-input)
         ("C-\\" . selectrum-next-candidate))
   :custom
