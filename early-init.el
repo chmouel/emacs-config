ba029e4540c662afbb7e67e8d4f3916a7ace4013
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-19 05:15:43 +0100

    use-file-dialog nil

--- early-init.el
+++ early-init.el
@@ -43,11 +43,13 @@ Deactivate this advice with:
         (height . 43)))
 
 (setq frame-inhibit-implied-resize t)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
 
+(setq use-file-dialog nil)
 (setq inhibit-splash-screen t)
 
 (setq package-quickstart t)
