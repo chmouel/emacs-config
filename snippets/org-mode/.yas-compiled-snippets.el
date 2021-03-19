d02330c837bb94f992bf6e25b504bac3a1e33eb4
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-19 12:53:50 +0100

    remove yas-compiled-snippets

--- snippets/org-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `org-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'org-mode
-		     '(("_yaml" "#+BEGIN_SRC yaml\n${0}\n#+END_SRC" "yamlsrc" nil nil nil "/Users/chmouel/.emacs.d/snippets/org-mode/yamlsrc" nil nil)
-		       ("_js" "#+BEGIN_SRC json\n{\n$0\n}\n#+END_SRC" "_js" nil nil nil "/Users/chmouel/.emacs.d/snippets/org-mode/_js" nil nil)))
-
-
-;;; Do not edit! File generated at Wed Feb 24 15:49:22 2021
