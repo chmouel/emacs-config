758217c55e83589086e4e8c1b7bddd1ff3509d0a
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-19 23:09:37 +0100

    use emacs init time function

--- init.el
+++ init.el
@@ -4,16 +4,13 @@
 
 
 ;; Startup time
-(defun efs/display-startup-time ()
+(defun chmou-display-startup-time ()
   (message
    "Emacs loaded in %s with %d garbage collections."
-   (format
-    "%.2f seconds"
-    (float-time
-     (time-subtract after-init-time before-init-time)))
+   (emacs-init-time)
    gcs-done))
 
-(add-hook 'emacs-startup-hook #'efs/display-startup-time)
+(add-hook 'emacs-startup-hook #'chmou-display-startup-time)
 
 
 ;;----------------------------------------------------------------------------
