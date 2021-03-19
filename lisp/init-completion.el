66b5c7e883881004e5c42638e716588a1b11fd1c
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-19 05:25:21 +0100

    readd embark

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -106,6 +106,28 @@
   ("C-c U" . consult-git-grep)
   ("M-g M-g" . consult-goto-line))
 
+(use-package embark
+  :bind
+  ("M-o" . embark-act)
+  :config
+  ;; For Selectrum users:
+  (defun current-candidate+category ()
+    (when selectrum-active-p
+      (cons (selectrum--get-meta 'category)
+            (selectrum-get-current-candidate))))
+  (add-hook 'embark-target-finders #'current-candidate+category)
+
+  (defun current-candidates+category ()
+    (when selectrum-active-p
+      (cons (selectrum--get-meta 'category)
+            (selectrum-get-current-candidates
+             ;; Pass relative file names for dired.
+             minibuffer-completing-file-name))))
+
+  (add-hook 'embark-candidate-collectors #'current-candidates+category)
+  ;; No unnecessary computation delay after injection.
+  (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate))
+
 (use-package selectrum
   :hook (after-init . selectrum-mode)
   :preface (declare-function chmou-selectrum-insert-or-submit-current-candidate nil)
