231ac5b727404bb3abb2da9b730fc485bd19627f
Chmouel Boudjnah <chmouel@chmouel.com>
2021-03-23 15:13:04 +0100

    remove my-sh-mode-hook: useless and relic from antic

--- lisp/init-sh.el
+++ lisp/init-sh.el
@@ -1,8 +1,7 @@
 (use-package sh-script
-  :hook
-  (sh-mode . my-sh-mode-hook)
   :config
-  (add-to-list 'multi-compile-alist '(sh-mode . (("bash-debug" . "bash -n \"%path\"")
-                                                 ("bash-run" . "bash \"%path\"")))))
+  (add-to-list 'multi-compile-alist
+               '(sh-mode . (("bash-debug" . "bash -n \"%path\"")
+                            ("bash-run" . "bash \"%path\"")))))
 
 (provide 'init-sh)
