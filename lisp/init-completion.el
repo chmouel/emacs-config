8284efc891edd2fe3e64d624af4e5adba9db0085
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-16 08:34:04 +0200

    Remove embark hook

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -77,25 +77,7 @@
 
 (use-package embark
   :bind
-  ("M-o" . embark-act)
-  :config
-  ;; For Selectrum users:
-  (defun current-candidate+category ()
-    (when selectrum-active-p
-      (cons (selectrum--get-meta 'category)
-            (selectrum-get-current-candidate))))
-  (add-hook 'embark-target-finders #'current-candidate+category)
-
-  (defun current-candidates+category ()
-    (when selectrum-active-p
-      (cons (selectrum--get-meta 'category)
-            (selectrum-get-current-candidates
-             ;; Pass relative file names for dired.
-             minibuffer-completing-file-name))))
-
-  (add-hook 'embark-candidate-collectors #'current-candidates+category)
-  ;; No unnecessary computation delay after injection.
-  (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate))
+  ("M-o" . embark-act))
 
 (use-package embark-consult
   :after (embark consult)
