685c55b2a3b7819f2542889f6ac26bb72de0f214
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-20 07:37:49 +0200

    more modifications

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -1,12 +1,14 @@
 ;; Enable vertico
 (use-package vertico
   :custom-face
-  (vertico-current ((t ())))
+  (vertico-current ((t (:foreground "light salmon"))))
   :custom
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles . (partial-completion)))))
   (vertico-count-format '())
   (completion-styles '(basic partial-completion emacs22 substring flex))
+  :config
+  (define-key vertico-map (kbd "C-s") #'vertico-next)
   :init
   (vertico-mode))
 
@@ -14,9 +16,10 @@
 ;; Enable `partial-completion' for files to allow path expansion.
 ;; You may prefer to use `initials' instead of `partial-completion'.
 (use-package orderless
-  :disabled
+  :demand t
   :init
   (setq completion-styles '(orderless)
         completion-category-defaults nil
-        completion-category-overrides '((file (styles . (partial-completion))))))
+        completion-category-overrides '((file (styles . (partial-completion flex))))))
+
 (provide 'init-vertico)
