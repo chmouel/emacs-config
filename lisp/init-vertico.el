55b22c0e015a902a18c68e32251ea38dec6535be
Chmouel Boudjnah <chmouel@chmouel.com>
2021-04-27 14:03:12 +0200

    remove orderless from here

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -10,16 +10,4 @@
   :init
   (vertico-mode))
 
-(use-package orderless
-  :demand t
-  :custom
-  (completion-styles '(orderless))
-  (orderless-matching-styles '(orderless-literal orderless-regexp orderless-initialism orderless-flex))
-  (orderless-component-separator "`")
-  (completion-category-defaults nil)
-  (completion-category-overrides
-   '((file (styles basic flex initials substring))
-     (buffer (styles basic flex initials substring))
-     (info-menu (styles basic flex initials substring)))))
-
 (provide 'init-vertico)
