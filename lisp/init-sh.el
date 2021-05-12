f4382218247fc1bbc99d29de353b55f0f12ecc57
Chmouel Boudjnah <chmouel@chmouel.com>
2021-05-12 11:47:07 +0200

    Don't use lsp for sh
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- lisp/init-sh.el
+++ lisp/init-sh.el
@@ -1,9 +1,3 @@
-(use-package sh-script
-  :hook ((sh-mode . lsp))
-  ;; :config
-  ;; (add-to-list 'multi-compile-alist
-  ;;              '(sh-mode . (("bash-debug" . "bash -n \"%path\"")
-  ;;                           ("bash-run" . "bash \"%path\""))))
-  )
+(use-package sh-script)
 
 (provide 'init-sh)
