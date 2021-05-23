20759e0809d4aadb25f986e4c1a5dd699a9b62af
Chmouel Boudjnah <chmouel@chmouel.com>
2021-05-23 09:39:32 +0200

    Better ivy

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -16,12 +16,26 @@
   (ivy-count-format "")
   (ivy-use-virtual-buffers t)
   :config
-  (setq ivy-height 10
-        ivy-wrap nil
-        ivy-re-builders-alist '((t . orderless-ivy-re-builder))
-        ivy-count-format ""
-        ivy-use-virtual-buffers t))
+  (setq
+   ivy-height 15
+   ivy-on-del-error-function #'ignore
+   ivy-initial-inputs-alist nil
+   ivy-fixed-height-minibuffer t
+   ivy-wrap nil
+   ivy-count-format ""
+   ivy-use-virtual-buffers t))
 
+(use-package ivy-rich
+  :hook ((counsel-projectile-mode . ivy-rich-mode) ; MUST after `counsel-projectile'
+         (ivy-rich-mode . (lambda ()
+                            "Use abbreviate in `ivy-rich-mode'."
+                            (setq ivy-virtual-abbreviate
+                                  (or (and ivy-rich-mode 'abbreviate) 'name)))))
+  :init
+  ;; For better performance
+  (setq ivy-rich-parse-remote-buffer nil))
+
+;; Additional key bindings for Ivy
 (use-package all-the-icons-ivy
   :after (all-the-icons ivy)
   :custom
@@ -38,9 +52,61 @@
   :config (all-the-icons-ivy-setup))
 
 (use-package ivy-prescient
-  :disabled
-  :ensure t
-  :config
+  :commands ivy-prescient-re-builder
+  :custom-face
+  (ivy-minibuffer-match-face-1 ((t (:inherit font-lock-doc-face :foreground nil))))
+  :init
+  (defun ivy-prescient-non-fuzzy (str)
+    "Generate an Ivy-formatted non-fuzzy regexp list for the given STR.
+This is for use in `ivy-re-builders-alist'."
+    (let ((prescient-filter-method '(literal regexp)))
+      (ivy-prescient-re-builder str)))
+
+  (setq ivy-prescient-retain-classic-highlighting t
+        ivy-re-builders-alist
+        '((counsel-unicode-char . ivy-prescient-non-fuzzy)
+          (t . ivy-prescient-re-builder))
+        ivy-prescient-sort-commands
+        '(:not swiper swiper-isearch ivy-switch-buffer
+               lsp-ivy-workspace-symbol ivy-resume ivy--restore-session
+               counsel-grep counsel-git-grep counsel-rg counsel-ag
+               counsel-ack counsel-fzf counsel-pt counsel-imenu
+               counsel-org-capture counsel-load-theme counsel-yank-pop
+               counsel-recentf counsel-buffer-or-recentf
+               centaur-load-theme))
   (ivy-prescient-mode 1))
+
+(use-package ivy-posframe
+  :custom-face
+  (ivy-posframe-border ((t (:background ,(face-foreground 'font-lock-comment-face)))))
+  :hook (ivy-mode . ivy-posframe-mode)
+  :init
+  (setq ivy-posframe-border-width 3
+        ivy-posframe-parameters
+        `((background-color . ,(face-background 'tooltip))))
+
+  :config
+  (ivy-posframe-mode)
+  (add-hook 'after-load-theme-hook
+            (lambda ()
+              (posframe-delete-all)
+              (custom-set-faces
+               `(ivy-posframe-border
+                 ((t (:background ,(face-foreground 'font-lock-comment-face))))))
+              (setf (alist-get 'background-color ivy-posframe-parameters)
+                    (face-background 'tooltip))))
+
+  (with-no-warnings
+    (defun ivy-display-at-frame-center-near-bottom-fn (str)
+      (ivy-posframe--display str #'ivy-poshandler-frame-center-near-bottom-fn))
+
+    (defun ivy-poshandler-frame-center-near-bottom-fn (info)
+      (let ((parent-frame (plist-get info :parent-frame))
+            (pos (posframe-poshandler-frame-center info)))
+        (cons (car pos)
+              (truncate (/ (frame-pixel-height parent-frame) 2)))))
+
+    (setf (alist-get t ivy-posframe-display-functions-alist)
+          #'ivy-display-at-frame-center-near-bottom-fn)))
 
 (provide 'init-ivy)
