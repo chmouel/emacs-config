02fa06d00cb0d0384e92c07cded226c095bf8131
Chmouel Boudjnah <chmouel@chmouel.com>
2021-10-12 06:24:24 +0200

    remove c-c o from this

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -5,16 +5,10 @@
   (let ((project-switch-commands 'project-dired))
     (call-interactively 'project-switch-project)))
 
-(defun my-project-and-find-file()
-  (interactive)
-  (let ((project-switch-commands 'project-find-file))
-    (call-interactively 'project-switch-project)))
-
 (use-package project
   :ensure t
   :bind
   ("C-c b" . project-switch-to-buffer)
-  ("C-c o" . my-project-and-find-file)
   ("C-c p" . my-project-and-dired)
   ("C-c f" . project-find-file)
   ("C--"   . project-find-file)
