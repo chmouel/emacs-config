f5c863a41e41a3da0921d5324aec9e972ded1dde
Chmouel Boudjnah <chmouel@chmouel.com>
2021-10-12 22:27:10 +0200

    another theme today

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -25,7 +25,7 @@
    ((string= (my-short-hostname) "thebatman")
     (load-theme 'doom-one))
    ((string= (my-short-hostname) "ibra")
-    (load-theme 'doom-outrun-electric))
+    (load-theme 'doom-moonlight))
    ((string= (my-short-hostname) "windaube")
     (load-theme 'doom-opera-light))))
 
