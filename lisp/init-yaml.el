21ef149d3097ec12f03f203aaeab64045caaef34
Chmouel Boudjnah <chmouel@chmouel.com>
2021-10-15 08:22:24 +0200

    outline-minor-mode

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -1,10 +1,12 @@
 (use-package yaml-mode :ensure t
   :hook
+  (yaml-mode . outline-minor-mode)
   (yaml-mode . format-all-mode)
   (yaml-mode . highlight-indentation-mode)
   (yaml-mode . electric-pair-local-mode)
   (yaml-mode . highlight-indentation-mode)
   :config
+  (setq-local outline-regexp "^ *\\([A-Za-z0-9_-]*: *[>|]?$\\|-\\b\\)")
   (font-lock-add-keywords
    'yaml-mode
    '(("\\($(\\(workspaces\\|context\\|params\\)\.[^)]+)\\)" 1 'font-lock-constant-face prepend)
@@ -12,6 +14,7 @@
   :bind
   (:map yaml-mode-map
         ("C-<return>" . imenu-list-smart-toggle)
+        ("<backtab>" . outline-toggle-children)
         ("C--" . (lambda () (interactive) (call-interactively 'highlight-indentation-current-column-mode)))
         ("C-S-f" . format-all-buffer)
         ("C-=" . whitespace-cleanup)))
