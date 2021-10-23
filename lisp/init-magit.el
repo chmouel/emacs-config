a10509b6d6ecd7074504bc57789b04d7d8cca445
Chmouel Boudjnah <chmouel@chmouel.com>
2021-10-23 23:22:19 +0200

    Add signed-off automatically
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,6 @@
 (use-package magit
   :commands (magit-read-repository magit-toplevel)
-  :bind (("C-x v s-v" . my-magit-commit-buffer)
+  :bind (("C-x v V" . my-magit-commit-buffer)
          ("C-x v -" . magit-pull-from-pushremote)
          ("C-x v =" . magit-diff-buffer-file)
          ("C-x v l" . magit-log-buffer-file)
@@ -46,6 +46,13 @@
     (local-set-key '[(control =)] 'magit-commit-create))
   (global-git-commit-mode))
 
+
+(use-package git-commit
+  :config
+  (add-to-list
+   'git-commit-setup-hook
+   (lambda () (interactive "P")
+     (call-interactively 'git-commit-signoff))))
 
 (use-package smerge-mode
   :bind ("s-m" . hydra-smerge/body)
