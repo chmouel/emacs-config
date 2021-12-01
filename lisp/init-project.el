b89a330b10db4f1492e8904a7aa672aa39c81d7b
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-01 10:34:37 +0100

    inlining

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,20 +1,16 @@
 ;; Projects
-
-(defun my-project-and-dired()
-  (interactive)
-  (let ((project-switch-commands 'project-dired))
-    (call-interactively 'project-switch-project)))
-
-(defun my-project-and-ff()
-  (interactive)
-  (let ((project-switch-commands 'project-find-file))
-    (call-interactively 'project-switch-project)))
-
 (use-package project
   :ensure t
   :bind
   ("C-c b" . project-switch-to-buffer)
-  ("C-x p o" . my-project-and-ff)
+  ("C-x p o" . (lambda ()
+                 (interactive)
+                 (let ((project-switch-commands 'project-find-file))
+                   (call-interactively 'project-switch-project))))
+  ("C-x p p" . (lambda ()
+                 (interactive)
+                 (let ((project-switch-commands 'project-dired))
+                   (call-interactively 'project-switch-project))))
   ("C-x p p" . my-project-and-dired)
   ("C-x p P" . project-switch-project)
   ("C-c f" . project-find-file)
