636cf47cc256d8f795bb2a6cd2fa3bf387609b74
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-09 22:36:04 +0100

    add project-x if exist
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -17,4 +17,16 @@
   :custom
   (project-list-file (locate-user-emacs-file "auto-save-list/project-list.file")))
 
+(defvar project-x-location
+  (expand-file-name
+   (concat "auto-save-list/modes/project-x") user-emacs-directory))
+(use-package project-x
+  :after project
+  :if (file-directory-p project-x-location)
+  :load-path project-x-location
+  :init
+  (require 'project-x)
+  (setq project-x-save-interval 600)    ;Save project state every 10 min
+  (project-x-mode 1)) 
+
 (provide 'init-project)
