d927205dde311c3bff17e2d4a27a1350b236de93
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-14 12:26:40 +0100

    init elisp

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -13,10 +13,27 @@
   (emacs-lisp-mode . aggressive-indent-mode)
   (emacs-lisp-mode . company-mode)
   (emacs-lisp-mode . turn-on-eldoc-mode)
+  :bind (:map emacs-lisp-mode-map
+              ("C-x C-e" . eval-buffer)))
+
+
+(use-package slime-company
+  :hook (slime-mode . company-mode))
+
+(use-package lisp
+  :ensure nil
+  :defer t
+  :interpreter ("emacs" . emacs-lisp-mode)
+  :config
+  :hook
+  (lisp-mode . aggressive-indent-mode)
+  (lisp-mode . company-mode)
+  (lisp-mode . turn-on-eldoc-mode)
   (lisp-interaction-mode . turn-on-eldoc-mode)
   (ielm-mode . turn-on-eldoc-mode)
   :bind (:map emacs-lisp-mode-map
               ("C-x C-e" . eval-buffer)))
+
 
 (use-package scheme
   :hook
