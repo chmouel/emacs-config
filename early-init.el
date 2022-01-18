223e7d7dcc2266ca83b4cec24399ac99fafbaa29
Chmouel Boudjnah <chmouel@chmouel.com>
2022-01-18 20:03:38 +0100

    save all backups to $TMPDIR

--- early-init.el
+++ early-init.el
@@ -1,6 +1,15 @@
 (setq mac-option-modifier 'meta)
 (setq mac-command-modifier 'super)
 
+;; Save all tempfiles in $TMPDIR/emacs$UID/                                                        
+(defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
 ;;
 ;; Buffer *Messages* avec timestamp
 ;;
