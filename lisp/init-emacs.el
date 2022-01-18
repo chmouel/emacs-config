fae550322fa2e558f547121d881d11fb4024bf5f
Chmouel Boudjnah <chmouel@chmouel.com>
2022-01-18 22:01:29 +0100

    set backup by default!

--- lisp/init-emacs.el
+++ lisp/init-emacs.el
@@ -77,7 +77,21 @@
   ;; Backup
   (vc-follow-symlinks t)
   (enable-local-variables :all) ;; Is it a good idea ?? probably not :\
-  (make-backup-files nil))
+  (version-control t)
+  (vc-make-backup-files t)
+  (backup-by-copying t)
+  (delete-old-versions t)
+  (kept-new-versions 6)
+  (kept-old-versions 2)
+  (create-lockfiles nil)
+  ;; dont backup files opened by sudo
+  (backup-enable-predicate
+   (lambda (name)
+     (and (normal-backup-enable-predicate name)
+          (not
+           (let ((method (file-remote-p name 'method)))
+             (when (stringp method)
+               (member method '("su" "sudo" "doas")))))))))
 
 ;; Diff
 (use-package diff
@@ -270,28 +284,28 @@
   :ensure nil
   :custom
   (calendar-holidays `((holiday-fixed 1 1 "Jour de l'an")
-	                   (holiday-fixed 1 6 "Épiphanie")
-	                   (holiday-fixed 2 2 "Chandeleur")
-	                   (holiday-fixed 2 14 "Saint Valentin")
-	                   (holiday-fixed 5 1 "Fête du travail")
-	                   (holiday-fixed 5 8 "Commémoration de la capitulation de l'Allemagne en 1945")
-	                   (holiday-fixed 6 21 "Fête de la musique")
-	                   (holiday-fixed 7 14 "Fête nationale - Prise de la Bastille")
-	                   (holiday-fixed 8 15 "Assomption (Religieux)")
-	                   (holiday-fixed 11 11 "Armistice de 1918")
-	                   (holiday-fixed 11 1 "Toussaint")
-	                   (holiday-fixed 11 2 "Commémoration des fidèles défunts")
-	                   (holiday-fixed 12 25 "Noël")
+                       (holiday-fixed 1 6 "Épiphanie")
+                       (holiday-fixed 2 2 "Chandeleur")
+                       (holiday-fixed 2 14 "Saint Valentin")
+                       (holiday-fixed 5 1 "Fête du travail")
+                       (holiday-fixed 5 8 "Commémoration de la capitulation de l'Allemagne en 1945")
+                       (holiday-fixed 6 21 "Fête de la musique")
+                       (holiday-fixed 7 14 "Fête nationale - Prise de la Bastille")
+                       (holiday-fixed 8 15 "Assomption (Religieux)")
+                       (holiday-fixed 11 11 "Armistice de 1918")
+                       (holiday-fixed 11 1 "Toussaint")
+                       (holiday-fixed 11 2 "Commémoration des fidèles défunts")
+                       (holiday-fixed 12 25 "Noël")
                        ;; fetes a date variable
-	                   (holiday-easter-etc 0 "Pâques")
+                       (holiday-easter-etc 0 "Pâques")
                        (holiday-easter-etc 1 "Lundi de Pâques")
                        (holiday-easter-etc 39 "Ascension")
                        (holiday-easter-etc 49 "Pentecôte")
                        (holiday-easter-etc -47 "Mardi gras")
-	                   (holiday-float 5 0 4 "Fête des mères")
-	                   ;; dernier dimanche de mai ou premier dimanche de juin si c'est le
-	                   ;; même jour que la pentecôte TODO
-	                   (holiday-float 6 0 3 "Fête des pères"))) ;; troisième dimanche de juin
+                       (holiday-float 5 0 4 "Fête des mères")
+                       ;; dernier dimanche de mai ou premier dimanche de juin si c'est le
+                       ;; même jour que la pentecôte TODO
+                       (holiday-float 6 0 3 "Fête des pères"))) ;; troisième dimanche de juin
   (calendar-mark-holidays-flag t)
   (calendar-week-start-day 1))
 
