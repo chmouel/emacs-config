0d9f8cb204a603d9a014fa988ae61fb795b1edf3
Chmouel Boudjnah <chmouel@chmouel.com>
2022-04-15 15:34:39 +0200

    orderless was too confusing back to normal completionstyles

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -1,19 +1,16 @@
-(use-package orderless
-  :demand t
-  :custom
-  (completion-styles '(orderless))
-  (orderless-matching-styles
-   '(orderless-flex
-     orderless-regexp
-     orderless-prefixes
-     orderless-literal))
-  (orderless-component-separator " ")
-  (completion-category-defaults nil))
-
 ;; Enable vertico
 (use-package vertico
   :custom
   (vertico-count-format '())
+  :config
+  (setq completion-styles
+        '(partial-completion substring initials flex))
+
+  (setq completion-category-overrides
+        '((file (styles . (partial-completion substring)))
+          (buffer (styles . ( basic substring partial-completion)))
+          (project-file (styles . (partial-completion substring)))
+          (info-menu (styles . (substring)))))
   :bind
   (:map vertico-map
         ("C-s" . vertico-next)
@@ -27,14 +24,27 @@
   (vertico-reverse-mode t)
   (vertico-mode t))
 
-(use-package vertico-directory
-  :ensure nil
-  :after vertico
-  :bind (:map vertico-map
-              ("DEL"   . vertico-directory-delete-char)
-              ("M-DEL" . vertico-directory-delete-word)
-              ("C-w"   . vertico-directory-delete-word)
-              ("RET"   . vertico-directory-enter)))
+;; (use-package vertico-directory
+;;   :ensure nil
+;;   :after vertico
+;;   :bind (:map vertico-map
+;;               ("DEL"   . vertico-directory-delete-char)
+;;               ("M-DEL" . vertico-directory-delete-word)
+;;               ("C-w"   . vertico-directory-delete-word)
+;;               ("RET"   . vertico-directory-enter)))
+;; (use-package orderless
+;;   :demand t
+;;   :custom
+;;   (completion-styles '(orderless))
+;;   (orderless-matching-styles
+;;    '(orderless-flex
+;;      orderless-regexp
+;;      orderless-prefixes
+;;      orderless-literal))
+;;   (orderless-component-separator " ")
+;;   (completion-category-defaults nil))
+
+
 
 
 (provide 'init-vertico)
