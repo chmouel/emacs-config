f8c587e57cc81d60f2cc4808b38dda9f7ff74971
Chmouel Boudjnah <chmouel@chmouel.com>
2022-05-08 09:22:23 +0200

    add regexp to vertico

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -6,21 +6,13 @@
   (setq orderless-matching-styles
         '(orderless-initialism
           orderless-prefixes
-          orderless-literal)))
+          orderless-literal
+          orderless-regexp)))
 
 ;; Enable vertico
 (use-package vertico
   :custom
   (vertico-count-format '())
-  ;; :config
-  ;; (setq completion-styles
-  ;;       '(partial-completion substring initials substring flex))
-
-  ;; (setq completion-category-overrides
-  ;;       '((file (styles . (partial-completion substring)))
-  ;;         (buffer (styles . ( basic substring partial-completion)))
-  ;;         (project-file (styles . (partial-completion substring)))
-  ;;         (info-menu (styles . (substring)))))
   :bind
   (:map vertico-map
         ("C-s" . vertico-next)
@@ -34,25 +26,14 @@
   (vertico-reverse-mode t)
   (vertico-mode t))
 
-;; (use-package vertico-directory
-;;   :ensure nil
-;;   :after vertico
-;;   :bind (:map vertico-map
-;;               ("DEL"   . vertico-directory-delete-char)
-;;               ("M-DEL" . vertico-directory-delete-word)
-;;               ("C-w"   . vertico-directory-delete-word)
-;;               ("RET"   . vertico-directory-enter)))
-;; (use-package orderless
-;;   :demand t
-;;   :custom
-;;   (completion-styles '(orderless))
-;;   (orderless-matching-styles
-;;    '(orderless-flex
-;;      orderless-regexp
-;;      orderless-prefixes
-;;      orderless-literal))
-;;   (orderless-component-separator " ")
-;;   (completion-category-defaults nil))
+(use-package vertico-directory
+  :ensure nil
+  :after vertico
+  :bind (:map vertico-map
+              ("DEL"   . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word)
+              ("C-w"   . vertico-directory-delete-word)
+              ("RET"   . vertico-directory-enter)))
 
 
 
