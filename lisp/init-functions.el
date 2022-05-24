d4a46f017a84c087fb93d98718caa884ac68839c
Chmouel Boudjnah <chmouel@chmouel.com>
2022-05-24 17:09:31 +0200

    swap it!

--- lisp/init-functions.el
+++ lisp/init-functions.el
@@ -134,5 +134,19 @@ have more than 3 then well you better start using resize-window.el "
   (select-window (get-largest-window)))
 (global-set-key (read-kbd-macro "C-c o") 'my-precious-layouts)
 
+(defun my-window-split-toggle ()
+  "Toggle between horizontal and vertical split with two windows."
+  (interactive)
+  (if (> (length (window-list)) 2)
+      (error "Can't toggle with more than 2 windows!")
+    (let ((func (if (window-full-height-p)
+                    #'split-window-vertically
+                  #'split-window-horizontally)))
+      (delete-other-windows)
+      (funcall func)
+      (save-selected-window
+        (other-window 1)
+        (switch-to-buffer (other-buffer))))))
+(global-set-key (read-kbd-macro "C-c <DEL>") 'my-window-split-toggle)
 
 (provide 'init-functions)
